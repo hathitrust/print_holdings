70960a0e9a16971b99e5dd1c0215ffcd872b5ffe
Martin Warin <mwarin@umich.edu>
2014-01-10 10:39:12 -0500

    minor tweak, using hathidata for output

--- report/common/member_counts/oclc_matchcount_query.rb
+++ report/common/member_counts/oclc_matchcount_query.rb
@@ -1,9 +1,9 @@
 require 'hathidb';
 require 'hathilog';
+require 'hathidata';
 
-db = Hathidb::Db.new();
+db  = Hathidb::Db.new();
 dbh = db.get_conn(); 
-
 log = Hathilog::Log.new();
 
 # Get number of MATCHING oclc numbers per member.
@@ -15,15 +15,14 @@ log = Hathilog::Log.new();
 outer_query = "SELECT DISTINCT member_id FROM holdings_memberitem";
 inner_query = dbh.prepare("SELECT COUNT(distinct ho.oclc) AS numoclc FROM holdings_memberitem AS hm, holdings_htitem_oclc AS ho WHERE ho.oclc = hm.oclc and member_id = ?");
 
-File.open('matching_counts.tsv', 'w:utf-8') do |mc|
-  mc.sync = true;
-  log.d(outer_query);
-  dbh.query(outer_query) do |mid_row|
-    m = mid_row[:member_id];
-    inner_query.query(m) do |cnt_row|
-      count = cnt_row[:numoclc];
-      log.d("#{m} #{count}");
-      mc.puts [m.to_s, count.to_s].join("\t");
-    end
+hdf = Hathidata::Data.new('oclc_matchcounts.tsv').open('w');
+hdf.file.sync = true;
+log.d(outer_query);
+dbh.query(outer_query) do |mid_row|
+  m = mid_row[:member_id];
+  inner_query.query(m) do |cnt_row|
+    count = cnt_row[:numoclc];
+    log.d("#{m} #{count}");
+    hdf.file.puts [m.to_s, count.to_s].join("\t");
   end
 end
