54a60cee09fe8872d733a200ef02700ab9e8ec86
Martin Warin <mwarin@umich.edu>
2014-01-20 13:05:39 -0500

    Scripted step 01d

---
+++ load/build/reload_holdings_htitem.rb
@@ -0,0 +1,47 @@
+require 'hathidata';
+require 'hathidb';
+require 'hathilog';
+
+def get_loadfile (log)
+  loadfile = nil;
+  if ARGV.length > 0 then
+    loadfile = ARGV.shift;
+    log.d("Got filename #{loadfile} through ARGV");
+  else
+    loadfile = 'hathi_full_$ymd.data';
+    log.d("Assuming #{loadfile}");
+  end
+
+  hd = Hathidata::Data.new(loadfile);
+  if hd.exists? then
+    log.d("#{hd.path} exists.");
+    return hd.path.to_s;
+  else
+    log.e("Could not figure out which file to use.");
+  end
+  return nil;
+end
+
+if $0 == __FILE__ then
+  log = Hathilog::Log.new();
+  log.d("Started");
+
+  db   = Hathidb::Db.new();
+  conn = db.get_conn();
+
+  loadfile = get_loadfile(log);
+
+  if loadfile == nil then
+    raise "Failed";
+  end
+
+  [
+   "TRUNCATE holdings_htitem",
+   "LOAD DATA LOCAL INFILE '#{loadfile}' INTO TABLE holdings_htitem"
+  ].each do |q|
+    log.d(q);
+    conn.update(q);
+  end
+  conn.close();
+  log.d("Finished");
+end
