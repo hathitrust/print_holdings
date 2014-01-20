48279c92d8630ab3c72385c11080db6d387073a3
Martin Warin <mwarin@umich.edu>
2014-01-20 17:05:05 -0500

    specifying outfile in call, like pulintz did, not gonna mess around with the python code

--- load/build/step_02.sh
+++ load/build/step_02.sh
@@ -3,9 +3,10 @@
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd`
 popd > /dev/null
-DATAPATH="$SCRIPTPATH/../../data";
 
 data_dir="$SCRIPTPATH/../../data";
 latest_hathi_file=`ls -w1 $data_dir | egrep '^hathi_full_[0-9]+.txt$' | sort | tail -1`;
+ymd=`date +'%Y%m%d'`;
+mhoff_outfile="htitem_oclc.$ymd.data";
 
-python maketable_htitem_oclc_from_file.py $data_dir/$latest_hathi_file;
+python maketable_htitem_oclc_from_file.py $data_dir/$latest_hathi_file $data_dir/$mhoff_outfile;
