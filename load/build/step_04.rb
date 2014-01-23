8800cff321d7e1480b3d5e51e0f3dbd0f6040e61
Martin Warin <mwarin@umich.edu>
2014-01-23 10:46:31 -0500

    added comments and count-checks before and after update

--- load/build/step_04.rb
+++ load/build/step_04.rb
@@ -1,11 +1,12 @@
 require 'hathilog';
 require 'hathidb';
 
-# Copied from /htapps/pulintz.babel/Code/phdb/bin/update_htitem_multiparts.rb
-# and reeducated to comply with the new regime.
+# MW Jan 2014. Copied from:
+# /htapps/pulintz.babel/Code/phdb/bin/update_htitem_multiparts.rb
+# ... and reeducated to comply with the new regime.
 
-# This routine labels as 'multi' additional items associated with multipart
-# clusters that don't have any n_enum data.
+# This routine labels as 'multi' additional items associated with
+# multipart clusters that don't have any n_enum data.
 
 log  = Hathilog::Log.new();
 log.d("Started.");
@@ -13,38 +14,56 @@ db   = Hathidb::Db.new();
 conn = db.get_conn();
 
 # Preparing the queries.
+
+sql_check = %W<
+SELECT
+    item_type,
+    COUNT(*) AS c
+FROM
+    holdings_htitem
+GROUP BY
+    item_type;
+>.join(' ');
+query_check = conn.prepare(sql_check);
+
 # select multipart clusters
 sql_select_1 = %W<
-    SELECT
-        DISTINCT(cluster_id) AS cid
-    FROM
-        holdings_cluster_htitem_jn AS chtij,
-        holdings_htitem            AS h
-    WHERE
-        h.volume_id = chtij.volume_id
-        AND
-        h.item_type = 'multi'
+SELECT
+    DISTINCT(cluster_id) AS cid
+FROM
+    holdings_cluster_htitem_jn AS chtij,
+    holdings_htitem            AS h
+WHERE
+    h.volume_id = chtij.volume_id
+    AND
+    h.item_type = 'multi'
 >.join(' ');
 
 sql_select_2 = %W<
-    SELECT
-        DISTINCT(volume_id)
-    FROM
-        holdings_cluster_htitem_jn
-    WHERE
-        cluster_id = ?
+SELECT
+    DISTINCT(volume_id)
+FROM
+    holdings_cluster_htitem_jn
+WHERE
+    cluster_id = ?
 >.join(' ');
 query_select_2 = conn.prepare(sql_select_2);
 
 sql_update = %W<
-    UPDATE 
-        holdings_htitem 
-    SET 
-        item_type = 'multi' 
-    WHERE 
-        volume_id = ?
+UPDATE
+    holdings_htitem
+SET
+    item_type = 'multi'
+WHERE
+    volume_id = ?
 >.join(' ');
 query_update = conn.prepare(sql_update);
+
+log.d("Checking counts before...");
+log.d(sql_check);
+query_check.enumerate() do |row|
+  log.d("#{row[:item_type]}\t#{row[:c]}")
+end
 
 cluster_count = 0;
 log.d(sql_select_1);
@@ -62,6 +81,12 @@ conn.query(sql_select_1) do |row|
     log.d("#{cluster_count} distinct cluster_ids ...");
   end
 end
-conn.close();
 
+log.d("Checking counts after...");
+log.d(sql_check);
+query_check.enumerate() do |row|
+  log.d("#{row[:item_type]}\t#{row[:c]}")
+end
+
+conn.close();
 log.d("Finished");
