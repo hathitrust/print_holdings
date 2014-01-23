36f5a038fe9fae44162139e6f0c93eb712a12f14
Martin Warin <mwarin@umich.edu>
2014-01-23 15:24:20 -0500

    relative paths, copied scripts from pulintz, checking exit codes

--- load/build/step_08.sh
+++ load/build/step_08.sh
@@ -8,22 +8,41 @@ popd > /dev/null;
 # Combines all substeps of step 8 in the monthlies.
 
 date=`date +%Y%m%d`;
-datadir="/htapps/mwarin.babel/phdb_scripts/data";
+datadir=`readlink -e $SCRIPTPATH/../../data`;
+
 chmd="${datadir}/cluster_htmember_multi.${date}.data";
 hhhmd="${datadir}/holdings_htitem_htmember.multi.${date}.data";
 
-cmd1="jruby /htapps/mwarin.babel/phdb_scripts/load/build/relabel_mpm.rb";
-cmd2="jruby -J-Xmx2000m /htapps/pulintz.babel/Code/phdb/bin/multipart_cluster_mapper.rb $chmd";
-cmd3="jruby /htapps/pulintz.babel/Code/phdb/bin/generate_htitem_htmember_jn_data.rb $chmd $hhhmd";
+cmd1="jruby $SCRIPTPATH/relabel_mpm.rb";
+cmd2="jruby -J-Xmx2000m $SCRIPTPATH/multipart_cluster_mapper.rb $chmd";
+cmd3="jruby $SCRIPTPATH/generate_htitem_htmember_jn_data.rb $chmd $hhhmd";
 
-echo `date`; 
-echo "$cmd1"; 
+echo `date`;
+echo "$cmd1";
 $cmd1;
+exit_st=$?
+if [ $exit_st != 0 ]; then
+    echo "Exiting prematurely";
+    exit $exit_st;
+fi
 
-echo `date`; 
+echo `date`;
 echo "$cmd2"
 $cmd2;
+exit_st=$?
+if [ $exit_st != 0 ]; then
+    echo "Exiting prematurely";
+    exit $exit_st;
+fi
 
-echo `date`; 
-echo "$cmd3"; 
+echo `date`;
+echo "$cmd3";
 $cmd3;
+exit_st=$?
+if [ $exit_st != 0 ]; then
+    echo "Exiting prematurely";
+    exit $exit_st;
+fi
+
+echo `date`;
+echo 'Finished';
