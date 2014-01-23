db98678d1bec15174c8cf0a49f8bd99b0f70ac98
Martin Warin <mwarin@umich.edu>
2014-01-23 16:48:41 -0500

    use Multipart instead of PHDBMultiPartUtils

--- load/build/generate_htitem_htmember_jn_data.rb
+++ load/build/generate_htitem_htmember_jn_data.rb
@@ -1,49 +1,60 @@
-require 'phdb/phdb_utils'
+require 'hathidb';
+require 'hathidata';
+require 'hathilog';
 
+# Copied from
+# /htapps/pulintz.babel/Code/phdb/bin/generate_htitem_htmember_jn_data.rb
 
-# parses the output from 'multipart_cluster_mapper.rb' (typically a file      
-#   called "cluster_htmember_multi.2013xxxx.data" into a file suitable for         
-#   upload into the htitem_htmember_jn table.  The file is highly redundant;
-#   we'll let the DB deal with that based on the PK
-def parse_cluster_htmember_multi_datafile(infile, out_hhj) 
-  puts "#{Time.new()} START";
-  outf2 = File.open(out_hhj, 'w')
+# Parses the output from 'multipart_cluster_mapper.rb' (typically a file
+# called "cluster_htmember_multi.2013xxxx.data" into a file suitable for
+# upload into the htitem_htmember_jn table.  The file is highly redundant;
+# we'll let the DB deal with that based on the PK
 
-  members = PHDBUtils.get_member_list()
-  puts "#{members.length} members."
-  members.delete('ucm')
-  
-  linecount = 0
-  File.open(infile).each do |line|
-    linecount += 1
+def parse_cluster_htmember_multi_datafile(infile, out_hhj, log)
+  db = Hathidb::Db.new();
+  conn = db.get_conn();
+  get_members_sql = "SELECT DISTINCT member_id FROM holdings_htmember";
+  members = [];
+  conn.query(get_members_sql) do |row|
+    members << row[:member_id];
+  end
+  conn.close();
+  log.d("#{members.length} members.");
+  members.delete('ucm');
+
+  linecount = 0;
+  Hathidata.reader(infile) do |line|
+    linecount += 1;
     if linecount % 200000 == 0 then
-      puts "#{Time.new()} #{linecount}";
+      log.d(linecount);
     end
-    bits = line.chomp.split("\t")
-    if not members.include?(bits[2])
-      puts "Problem with member_id '#{bits[2]}'... skipping."
-      next
+    bits = line.chomp.split("\t");
+    if not members.include?(bits[2]) then
+      log.w("Problem with member_id '#{bits[2]}'... skipping.");
+      next;
     end
-    oclc = bits[0].to_i
-    if not oclc.is_a? Integer
-      puts "Problem with oclc '#{bits[0]}'... skipping."
-      next
+    oclc = bits[0].to_i;
+    if not oclc.is_a? Integer then
+      log.w("Problem with oclc '#{bits[0]}'... skipping.");
+      next;
     end
-    vol_bits = bits[4].split(",")
-    vol_bits.each do |vol|
-      counts = bits[5,5].join("\t")
-      outstr = "#{vol}\t#{bits[2]}\t#{counts}"
-      outf2.puts(outstr)
+    vol_bits = bits[4].split(",");
+    Hathidata.writer(out_hhj) do |hdout|
+      vol_bits.each do |vol|
+        counts = bits[5,5].join("\t");
+        hdout.file.puts("#{vol}\t#{bits[2]}\t#{counts}");
+      end
     end
-  end    
-  outf2.close
-  puts "#{Time.new()} FINISH";
+  end
 end
 
-
-
-if ARGV.length != 2
-  puts "Usage: ruby generate_htitem_htmember_jn_data.rb <infile> <htmj-outfile>\n"
-  exit
+if $0 == __FILE__ then
+  log = Hathilog::Log.new();
+  log.d("Started");
+  if ARGV.length != 2 then
+    log.e("Usage: ruby generate_htitem_htmember_jn_data.rb <infile> <htmj-outfile>");
+    exit 1;
+  end
+  parse_cluster_htmember_multi_datafile(ARGV[0], ARGV[1], log);
+  log.d("Finished");
 end
-parse_cluster_htmember_multi_datafile(ARGV[0], ARGV[1])
--- load/build/multipart_cluster_mapper.rb
+++ load/build/multipart_cluster_mapper.rb
@@ -1,55 +1,54 @@
-require 'phdb/phdb_utils'
-require 'phdb/phdb_multipart_utils'
-        
-     
+require 'hathidata';
+require 'hathidb';
+require 'hathilog';
+require 'multipart';
+
 # exports all lines for a cluster to an outfile (also puts them onscreen)
-# useful for debugging                        
-def get_cluster_htmember_multi_data(cluster_id, outfilen)
-  testfile = File.new(outfilen, "w")
-  multi_members_enum = PHDBMultipartUtils.get_multipart_members_list()
+# useful for debugging
+def get_cluster_htmember_multi_data(cluster_id, log)
+  log.d("Create cluster dump file for cluster_id #{cluster_id}");
+  multi_members_enum = Multipart.get_multipart_members_list();
   ## example cluster_id: 3220320
-  data = PHDBMultipartUtils.map_multipart_cluster_to_members(cluster_id, multi_members_enum)
-  data.each do |line|
-    puts line
-    testfile.puts(line)
+  data = Multipart.map_multipart_cluster_to_members(cluster_id, multi_members_enum);
+  Hathidata.write("cluster_#{cluster_id}") do |hdout|
+    data.each do |line|
+      puts line;
+      hdout.file.puts(line);
+    end
   end
-  testfile.close
 end
-       
-       
-def generate_cluster_htmember_multi_file(outfilen)
-  #outfile = File.new("cluster_htmember_multi.20120409.data", "w")
-  outfile = File.new(outfilen, "w")
-  multi_members_enum = PHDBMultipartUtils.get_multipart_members_list()
+
+def generate_cluster_htmember_multi_file(outfilen, log)
+  multi_members_enum = Multipart.get_multipart_members_list()
   puts "#{multi_members_enum.length} multipart members."
-  cids = PHDBMultipartUtils.get_multipart_cluster_list()
-  puts "#{cids.length} clusters in list."
+  cids = Multipart.get_multipart_cluster_list()
+  log.d("#{cids.length} clusters in list.");
   count = 0
-  cids.each do |cid|
-    result_set = PHDBMultipartUtils.map_multipart_cluster_to_members(cid, multi_members_enum)
-    # calc unique lines
-    result_set.uniq.each do |line|
-      # line: oclc, enum, member_id, cluster_id, volume_ids, count
-      outfile.puts(line)
-    end
-    count += 1
-    if ((count % 10000) == 0)
-      puts "#{count}..."
-    outfile.flush
+  Hathidata.write(outfilen) do |hdout|
+    cids.each do |cid|
+      result_set = Multipart.map_multipart_cluster_to_members(cid, multi_members_enum)
+      # calc unique lines
+      result_set.uniq.each do |line|
+        # line: oclc, enum, member_id, cluster_id, volume_ids, count
+        hdout.line.puts(line);
+      end
+      count += 1
+      if ((count % 10000) == 0)
+        puts "#{count}..."
+      end
     end
   end
-  outfile.close()
 end
 
-## testers ##
-#mems = PHDBMultipartUtils.get_multipart_members_list
-#puts mems
-#cluster_id = 3220320
-#outfile = "test.txt"
-#get_cluster_htmember_multi_data(cluster_id, outfile)
-
-if ARGV.length != 1
-  puts "Usage: ruby multipart_cluster_mapper.rb <outfile>\n"
-  exit
+if $0 == __FILE__ then
+  log = Hathilog::Log.new();
+  log.d("Started");
+  if ARGV.length != 1
+    log.e("Usage: ruby multipart_cluster_mapper.rb <outfile>");
+    exit 1;
+  end
+  # Test:
+  # get_cluster_htmember_multi_data(3220320, log);
+  generate_cluster_htmember_multi_file(ARGV[0], log);
+  log.d("Finished");
 end
-generate_cluster_htmember_multi_file(ARGV[0])
