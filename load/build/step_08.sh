b8d56dc910f094a7f7703aabc02cc296c8ef4d28
Martin Warin <mwarin@umich.edu>
2014-01-24 09:31:19 -0500

    added notes about expected running time

--- load/build/step_08.sh
+++ load/build/step_08.sh
@@ -13,8 +13,11 @@ datadir=`readlink -e $SCRIPTPATH/../../data`;
 chmd="${datadir}/cluster_htmember_multi.${date}.data";
 hhhmd="${datadir}/holdings_htitem_htmember.multi.${date}.data";
 
+# Fairly quick.
 cmd1="jruby $SCRIPTPATH/relabel_mpm.rb";
+# Takes ~9h.
 cmd2="jruby -J-Xmx2000m $SCRIPTPATH/multipart_cluster_mapper.rb $chmd";
+# Takes a couple of  minutes.
 cmd3="jruby $SCRIPTPATH/generate_htitem_htmember_jn_data.rb $chmd $hhhmd";
 
 echo `date`;
