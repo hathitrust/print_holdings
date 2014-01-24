2a3995af7e3bee9cd5df5048f44c9f5f6d648aa3
Martin Warin <mwarin@umich.edu>
2014-01-24 14:56:15 -0500

    using nicer paths

--- load/build/step_10e.sh
+++ load/build/step_10e.sh
@@ -1,6 +1,14 @@
 d=`date +'%Y%m%d'`;
-logfile="/htapps/mwarin.babel/phdb_scripts/load/build/step_10e.log";
-command="jruby /htapps/mwarin.babel/phdb_scripts/load/build/add_source_items_to_htitem_htmember_jn.rb /htapps/mwarin.babel/phdb_scripts/data/deposits.${d}.txt";
+
+pushd `dirname $0` > /dev/null;
+SCRIPTPATH=`pwd`;
+popd > /dev/null;
+
+datadir=`readlink -e $SCRIPTPATH/../../data`;
+logdir==`readlink -e $SCRIPTPATH/../../log`;
+logfile="$logdir/step_10e.log";
+
+command="jruby $SCRIPTPATH/add_source_items_to_htitem_htmember_jn.rb $datadir/deposits.${d}.txt";
 echo $command;
-echo "nohup and logging to $logfile"
+echo "nohup and logging to $logfile";
 nohup $command > $logfile &
