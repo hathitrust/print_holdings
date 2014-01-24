325b768c3f5dd7b4aedeadb7b13a1595509867a2
Martin Warin <mwarin@umich.edu>
2014-01-24 10:47:05 -0500

    Some of the cloud step prep can be scripted.

---
+++ load/build/step_09.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Combines scriptable substeps of step 9 (the cloud step) in the monthlies.
+
+pushd `dirname $0` > /dev/null;
+SCRIPTPATH=`pwd`;
+popd > /dev/null;
+
+date=`date +%Y%m%d`;
+datadir=`readlink -e $SCRIPTPATH/../../data`;
+
+input_current="s3://umich-lib-phdb-1/input/Current";
+ht003_dir="/htapps/pulintz.babel/data/phdb/HT003";
+
+# Empty the current input bucket.
+date;
+echo "DELeting old data.";
+s3cmd del $input_current/*;
+
+# Put current HT003 files in current input bucket.
+date;
+echo "PUTting new data.";
+s3cmd put $ht003_dir/HT003_*.tsv $input_current/;
