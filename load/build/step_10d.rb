849f7bf7efdd30eb645d70c62dc98f838fb50e2d
Martin Warin <mwarin@umich.edu>
2014-01-24 17:45:48 -0500

    more checks on result

--- load/build/step_10d.rb
+++ load/build/step_10d.rb
@@ -32,5 +32,18 @@ else
   log.w("#{queries[0]} should be 0, was #{results[0]}");
 end
 
+if results[3] > results[4] then
+  log.i("Result 3 & 4 check out, 3 is greater than 4.");
+else
+  log.w("#{queries[3]}\nshould be greater than #{queries[4]}\n... was not (#{results[3]} cmp #{results[4]}).");
+end
+
+
+if results[4] == results[5] then
+  log.i("Result 4 & 5 check out, they are the same.");
+else
+  log.w("#{queries[4]}\nand #{queries[5]}\n... should be the same, was different (#{results[4]} cmp #{results[5]}).");
+end
+
 conn.close();
 log.d("Finished.");
