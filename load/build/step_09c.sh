0acae1806358b375d08d0c3263613045f1d8e23e
Martin Warin <mwarin@umich.edu>
2014-01-24 13:40:48 -0500

    now checking for the _SUCCESS file after download.

--- load/build/step_09c.sh
+++ load/build/step_09c.sh
@@ -14,8 +14,18 @@ date;
 echo "Started";
 mkdir -pv $awsdir;
 
+echo "GETting files into $awsdir/";
 s3cmd get s3://umich-lib-phdb/output/$date/* $awsdir/;
 
+if [ -f $awsdir/_SUCCESS ]; then
+    echo "Looks like the MapReduce was a success.";
+else
+    echo "Looks like the MapReduce was a fail. Exiting.";
+    exit(1);
+fi
+
+date;
+echo "Loading files into DB.";
 ruby $SCRIPTPATH/load_awsdata.rb start $awsdir/;
 
 date;
