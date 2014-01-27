4f8f59c067ef0127d30ff33d4357e59875648729
Martin Warin <mwarin@umich.edu>
2014-01-27 14:58:41 -0500

    using build_lib.sh

--- load/build/step_01.sh
+++ load/build/step_01.sh
@@ -5,34 +5,28 @@ pushd `dirname $0` > /dev/null;
 SCRIPTPATH=`pwd`;
 popd > /dev/null;
 
+source $SCRIPTPATH/build_lib.sh;
+
 # Gets the "latest_hathi_file".
 ruby $SCRIPTPATH/hathi_grabber.rb;
-exit_st=$?
-if [ $exit_st != 0 ]; then
-    echo "Exiting prematurely";
-    exit $exit_st;
-fi
+exit_st=$?;
+check_exit_code $exit_st;
 
-data_dir="$SCRIPTPATH/../../data";
-latest_hathi_file=`ls -w1 $data_dir | egrep '^hathi_full_[0-9]+.txt$' | sort | tail -1`;
+latest_hathi_file=`ls -w1 $DATADIR | egrep '^hathi_full_[0-9]+.txt$' | sort | tail -1`;
 
 # Assuming this was manually placed here.
-serial_dir="$data_dir/serials";
-latest_serial_file=`ls -w1 $serial_dir | tail -1`;
+latest_serial_file=`ls -w1 $SERIALDIR | tail -1`;
 
 # Generate a hathi_full_YYYYMMDD.data file
 ruby $SCRIPTPATH/maketable_htitem_from_file.rb $latest_hathi_file serials/$latest_serial_file;
 
-exit_st=$?
-if [ $exit_st != 0 ]; then
-    echo "Exiting prematurely";
-    exit $exit_st;
-fi
+exit_st=$?;
+check_exit_code $exit_st;
 
 # Reload holdings_htitem with the output from the previous script.
 ruby reload_holdings_htitem.rb;
+exit_st=$?;
+check_exit_code $exit_st;
 
-exit_st=$?
-if [ $exit_st == 0 ]; then
-    echo "Finished with an OK exit code ($exit_st)";
-fi
+echo "Finished with an OK exit code ($exit_st)";
+
