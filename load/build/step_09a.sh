47b2f957e3adfa9e035bb861fad0310a767efb63
Martin Warin <mwarin@umich.edu>
2014-01-27 15:08:49 -0500

    using build_lib.sh

--- load/build/step_09a.sh
+++ load/build/step_09a.sh
@@ -5,9 +5,9 @@
 pushd `dirname $0` > /dev/null;
 SCRIPTPATH=`pwd`;
 popd > /dev/null;
+source $SCRIPTPATH/build_lib.sh;
 
 input_current="s3://umich-lib-phdb-1/input/Current";
-ht003_dir="/htapps/pulintz.babel/data/phdb/HT003";
 
 echo "Before:";
 s3cmd ls $input_current/;
@@ -20,7 +20,7 @@ s3cmd del $input_current/*;
 # Put current HT003 files in current input bucket.
 date;
 echo "PUTting new data.";
-s3cmd put $ht003_dir/HT003_*.tsv $input_current/;
+s3cmd put $HTDIR/HT003_*.tsv $input_current/;
 
 echo "After:";
 s3cmd ls $input_current/;
