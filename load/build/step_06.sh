523b196332885bed6e719afeec2327326a410967
Martin Warin <mwarin@umich.edu>
2014-01-27 15:03:31 -0500

    using build_lib.sh

--- load/build/step_06.sh
+++ load/build/step_06.sh
@@ -5,33 +5,32 @@ pushd `dirname $0` > /dev/null;
 SCRIPTPATH=`pwd`;
 popd > /dev/null;
 
+source $SCRIPTPATH/build_lib.sh;
 echo "Started `date`";
+
+#------------
 
 echo "6a started `date`";
 jruby $SCRIPTPATH/assign_cluster_type.rb;
 exit_st=$?
-if [ $exit_st != 0 ]; then
-    echo "Exiting prematurely";
-    exit $exit_st;
-fi
+check_exit_code $exit_st;
 echo "6a finished `date`";
+
+#------------
 
 echo "6b started `date`";
 jruby $SCRIPTPATH/num_items_cluster.rb;
 exit_st=$?
-if [ $exit_st != 0 ]; then
-    echo "Exiting prematurely";
-    exit $exit_st;
-fi
+check_exit_code $exit_st;
 echo "6b finished `date`";
 
+#------------
 echo "6c started `date`";
 jruby $SCRIPTPATH/calc_cluster_rights.rb;
 exit_st=$?
-if [ $exit_st != 0 ]; then
-    echo "Exiting prematurely";
-    exit $exit_st;
-fi
+check_exit_code $exit_st;
 echo "6c finished `date`";
+
+#------------
 
 echo 'Done.';
