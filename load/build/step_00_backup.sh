43677bc815fcc41dd8cc5aaa6843656d52eff6c5
Martin Warin <mwarin@umich.edu>
2014-01-27 14:34:29 -0500

    saving linecount of the HT00x-files

--- load/build/step_00_backup.sh
+++ load/build/step_00_backup.sh
@@ -8,9 +8,11 @@ pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd`
 popd > /dev/null
 DATAPATH="$SCRIPTPATH/../../data";
-BACKUPPATH="$DATAPATH/backup/";
+LOGPATH="$SCRIPTPATH/../../log/builds/current";
+BACKUPPATH="$DATAPATH/backup";
 
-mkdir -pv $BACKUPPATH;
+HT_DIR=/htapps/pulintz.babel/data/phdb/HT003;
+mkdir -pv $BACKUPPATH/;
 
 # Back up holdings_htitem_htmember_jn to file.
 echo "$SCRIPTPATH/backup_table.sh";
@@ -25,3 +27,6 @@ bash $SCRIPTPATH/backup_table.sh holdings_htitem_oclc;
 
 # Used to be 12b.
 bash $SCRIPTPATH/backup_table.sh holdings_H_counts
+
+# Remember the line counts for input.
+wc -l $HT_DIR/HT003_*.tsv > $LOGPATH/ht00x_file_linecounts.txt
