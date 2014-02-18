ba8c78eb3d4f006a1ee95701f3e46a76b5ded922
Martin Warin <mwarin@umich.edu>
2014-02-18 17:09:52 -0500

    no parens in bash exit!

--- load/build/step_09c.sh
+++ load/build/step_09c.sh
@@ -15,13 +15,13 @@ echo "Started";
 mkdir -pv $awsdir;
 
 echo "GETting files into $awsdir/";
-s3cmd get s3://umich-lib-phdb/output/$date/* $awsdir/;
+s3cmd get s3://umich-lib-phdb-1/output/$date/* $awsdir/;
 
 if [ -f $awsdir/_SUCCESS ]; then
     echo "Looks like the MapReduce was a success.";
 else
     echo "Looks like the MapReduce was a fail. Exiting.";
-    exit(1);
+    exit 1;
 fi
 
 date;
