9d43166897b3ba1931960c6665d451bbe597ffc1
Martin Warin <mwarin@umich.edu>
2014-02-24 12:55:22 -0500

    turned table name into var

--- load/build/step_10b.rb
+++ load/build/step_10b.rb
@@ -18,15 +18,18 @@ curmon = Time.new().strftime("%b").downcase();
 
 # Save holdings_htitem_htmember_jn_#{curmon} as a backup table,
 # and something we can use for computing deltas later.
-q1 = conn.prepare("RENAME TABLE holdings_htitem_htmember_jn TO holdings_htitem_htmember_jn_#{curmon}");
-q2 = conn.prepare("CREATE TABLE holdings_htitem_htmember_jn LIKE holdings_htitem_htmember_jn_#{curmon}");
-q3 = conn.prepare("LOAD DATA LOCAL INFILE '#{infile.path}' INTO TABLE holdings_htitem_htmember_jn");
+
+t  = 'holdings_htitem_htmember_jn';
+q1 = conn.prepare("RENAME TABLE #{t} TO #{t}_#{curmon}");
+q2 = conn.prepare("CREATE TABLE #{t} LIKE #{t}_#{curmon}");
+q3 = conn.prepare("LOAD DATA LOCAL INFILE '#{infile.path}' INTO TABLE #{t}");
 
 qx = 1;
 [q1, q2, q3].each do |qn|
   log.d("Running query #{qx}");
-  qx += 1;
+  log.d(qn);
   qn.execute();
+  qx += 1;
 end
 
 conn.close();
