e30a8a52d44ae7582cae36911ab0bbe70895791a
Martin Warin <mwarin@umich.edu>
2014-03-19 17:11:07 -0400

    not printing the %ser col anymore, since no one cares, and also printing the text file with tabs

--- report/common/cost/cost_report.rb
+++ report/common/cost/cost_report.rb
@@ -32,13 +32,13 @@ no_serials_list   = monograph_members - serial_members;
 
 # Average Cost Per Volume Calculations
 ave_cost_per_vol = CostCalc.calc_ave_cost_per_vol(total_operating_costs, conn);
-hdout.file.puts "Ave Cost Per Vol: #{ave_cost_per_vol}";
+hdout.file.puts "Ave Cost Per Vol\t#{ave_cost_per_vol}";
 
 ave_cost_per_vol_ic_spm = CostCalc.calc_adjusted_ic_spm_ave_cost_per_vol(ave_cost_per_vol, conn);
-hdout.file.puts "Adjusted Ave Cost Per IC SPM Vol: #{ave_cost_per_vol_ic_spm}";
+hdout.file.puts "Adjusted Ave Cost Per IC SPM Vol\t#{ave_cost_per_vol_ic_spm}";
 
 ave_cost_per_vol_ic_mpm = CostCalc.calc_adjusted_ic_mpm_ave_cost_per_vol(ave_cost_per_vol, conn);
-hdout.file.puts "Adjusted Ave Cost Per IC MPM Vol: #{ave_cost_per_vol_ic_mpm}";
+hdout.file.puts "Adjusted Ave Cost Per IC MPM Vol\t#{ave_cost_per_vol_ic_mpm}";
 
 ### public domain costs ###
 public_costs = CostCalc.calc_pd_costs(ave_cost_per_vol, conn);
@@ -50,12 +50,12 @@ per_member = sum/total_members;
 
 hdout.file.puts "Public domain costs:";
 hdout.file.puts "Calculating PD cost based on #{total_members} members.";
-hdout.file.puts "\tSinglepart Monos: $#{public_costs[0]}";
-hdout.file.puts "\tSerials: $#{public_costs[1]}";
-hdout.file.puts "\tMultipart Monos: $#{public_costs[2]}";
-hdout.file.puts "\tTotal public domain costs: $#{sum}";
-hdout.file.puts "\tPublic domain cost per member: $#{per_member}";
-hdout.file.puts "Remaining total cost: $#{total_operating_costs - sum}";
+hdout.file.puts "Singlepart Monos\t$#{public_costs[0]}";
+hdout.file.puts "Serials\t$#{public_costs[1]}";
+hdout.file.puts "Multipart Monos\t$#{public_costs[2]}";
+hdout.file.puts "Total public domain costs\t$#{sum}";
+hdout.file.puts "Public domain cost per member\t$#{per_member}";
+hdout.file.puts "Remaining total cost\t$#{total_operating_costs - sum}";
 
 ### monograph cost structure ###
 cost_struct = {};
@@ -82,10 +82,10 @@ monograph_members.each do |mmember|
   cost_struct[mmember][0] = result.to_f;
 end
 total_spm = "%.2f" % spm_total;
-hdout.file.puts "Target IC spm cost: $#{target_single_cost}";
-hdout.file.puts "Unadjusted IC SPM cost: $#{unadj_spm_total}";
-hdout.file.puts "Adjusted IC spm cost: $#{total_spm}";
-hdout.file.puts "Remaining total cost $#{total_operating_costs - sum - spm_total}";
+hdout.file.puts "Target IC spm cost\t$#{target_single_cost}";
+hdout.file.puts "Unadjusted IC SPM cost\t$#{unadj_spm_total}";
+hdout.file.puts "Adjusted IC spm cost\t$#{total_spm}";
+hdout.file.puts "Remaining total cost\t$#{total_operating_costs - sum - spm_total}";
 
 ### multipart monograph costs ###
 mpm_total = 0.0;
@@ -104,16 +104,16 @@ monograph_members.each do |mmember|
   cost_struct[mmember][1] = result.to_f;
 end
 total_mpm = "%.2f" % mpm_total;
-hdout.file.puts "Target IC multi cost: $#{target_multi_cost}";
-hdout.file.puts "Unadjusted Actual IC MPM cost: $#{unadj_mpm_total}";
-hdout.file.puts "Adjusted Actual IC mpm cost: $#{total_mpm}";
-hdout.file.puts "Remaining total cost $#{total_operating_costs - sum - spm_total - mpm_total}";
+hdout.file.puts "Target IC multi cost\t$#{target_multi_cost}";
+hdout.file.puts "Unadjusted Actual IC MPM cost\t$#{unadj_mpm_total}";
+hdout.file.puts "Adjusted Actual IC mpm cost\t$#{total_mpm}";
+hdout.file.puts "Remaining total cost\t$#{total_operating_costs - sum - spm_total - mpm_total}";
 
 ### serial costs ###
 hdout.file.puts "\nIn-copyright Serial costs:";
 ser1_total = CostCalc.calc_total_ic_serial_cost(ave_cost_per_vol, conn);
 total_ser1 = "%.2f" % ser1_total;
-hdout.file.puts "Target IC serial cost: $#{total_ser1}";
+hdout.file.puts "Target IC serial cost\t$#{total_ser1}";
 
 # monograph percents by member (to calculate serial percents)
 total_mono_cost = spm_total + mpm_total;
@@ -133,9 +133,9 @@ no_serial_sum_str = "%.2f" % no_serial_sum;
 amount_serial_remaining = ser1_total - no_serial_sum;
 hdout.file.puts "Calculating new ave cost per vol for ic serials...";
 new_ave_cost_per_vol_serial = CostCalc.calc_adjusted_ic_serial_cost_per_vol(ave_cost_per_vol, no_serial_sum, conn);
-hdout.file.puts "Total sum of non-serial member contributions using monograph percentage: $#{no_serial_sum_str}";
-hdout.file.puts "Remaining serial cost to be distributed amongst other members: $#{amount_serial_remaining}";
-hdout.file.puts "New average cost per volume for serials: $#{new_ave_cost_per_vol_serial}";
+hdout.file.puts "Total sum of non-serial member contributions using monograph percentage\t$#{no_serial_sum_str}";
+hdout.file.puts "Remaining serial cost to be distributed amongst other members\t$#{amount_serial_remaining}";
+hdout.file.puts "New average cost per volume for serials\t$#{new_ave_cost_per_vol_serial}";
 
 ### serial costs, calculated by serial cluster - full amount ###
 if calc_full_serial
@@ -155,7 +155,7 @@ if calc_full_serial
     cost_struct[smember][2] = result.to_f;
   end
   total_ser2 = "%.2f" % ser2_total;
-  hdout.file.puts "Total IC ser cost (full): $#{total_ser2}";
+  hdout.file.puts "Total IC ser cost (full)\t$#{total_ser2}";
   hdout.file.puts "Remaining total cost (full) $#{total_operating_costs - sum - spm_total - mpm_total - ser2_total}";
 end
 
@@ -177,17 +177,18 @@ serial_members.each do |smember|
 end
 
 total_ser3 = "%.2f" % ser3_total;
-hdout.file.puts "Total IC ser cost (reduced): $#{total_ser3}";
-hdout.file.puts "Remaining total cost (reduced): $#{total_operating_costs - sum - spm_total - mpm_total - ser3_total}";
+hdout.file.puts "Total IC ser cost (reduced)\t$#{total_ser3}";
+hdout.file.puts "Remaining total cost (reduced)\t$#{total_operating_costs - sum - spm_total - mpm_total - ser3_total}";
 hdout.close();
 
 ### produce member report ###
 Hathidata.write("costreport/costreport_$ymd.tsv") do |hdout|
-  hdout.file.puts "\nmember_id\tpd_cost\tspm_cost\tmpm_cost\t% ser\tserial-full\tserial-reduced";
+  hdout.file.puts "member_id\tpd_cost\tspm_cost\tmpm_cost\tserial-full\tserial-reduced";
   # k = member id, v = array of values.
   cost_struct.each do |k, v|
-    mem_ratio = percents[k];
-    hdout.file.puts [k, per_member, v[0], v[1], mem_ratio, v[2], v[3]].join("\t");
+    # Not using in report anymore, so dropped it from output.
+    #mem_ratio = percents[k];
+    hdout.file.puts [k, per_member, v[0], v[1], v[2], v[3]].join("\t");
   end
 end
 
