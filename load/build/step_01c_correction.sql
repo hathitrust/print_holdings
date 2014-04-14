1acef22651de19797dc6a185a72980e176168a6a
Martin Warin <mwarin@umich.edu>
2014-04-14 10:29:53 -0400

    copied over from pulintz

---
+++ load/build/step_01c_correction.sql
@@ -0,0 +1,36 @@
+DELETE FROM holdings_htitem WHERE volume_id = 'nyp.33433062731496';
+INSERT INTO holdings_htitem VALUES ('nyp.33433062731496', 'allow', 'pd', 8630988, NULL, 'NYP', '.b13778157x', '17285467', 'bib', '2010-10-03 19:30:31', 'mono', 0, NULL, NULL, 0, 1895, 'nyu', 'eng', 'BK');
+
+DELETE FROM holdings_htitem WHERE volume_id = 'mdp.39015078220921';
+INSERT INTO holdings_htitem VALUES ('mdp.39015078220921', 'deny', 'ic', 5343423, 'v.13:no.118-123(2008)', 'MiU', '005343423', '41448570', 'bib', '2013-01-04 19:30:31', 'multi', 0, '13:118-123', '2008', 0, 2008, 'le', 'ara', 'SE');
+
+-- DELETE FROM holdings_htitem WHERE volume_id = 'nyp.33433081645446';
+-- DELETE FROM holdings_htitem WHERE volume_id = 'inu.32000013031176';
+-- DELETE FROM holdings_htitem WHERE volume_id = 'uc1.32106020471998';
+-- DELETE FROM holdings_htitem WHERE volume_id = 'nyp.33433023168507';
+
+-- DELETE FROM holdings_htitem WHERE volume_id = 'nyp.33433068275159';
+-- DELETE FROM holdings_htitem WHERE volume_id = 'nyp.33433081678660';
+-- DELETE FROM holdings_htitem WHERE volume_id = 'nyp.33433009982327';
+-- DELETE FROM holdings_htitem WHERE volume_id = 'coo.31924095682286';
+
+
+-- DELETE FROM holdings_htitem WHERE volume_id = 'nyp.33433090762398';
+-- DELETE FROM holdings_htitem WHERE volume_id = 'nyp.33433090822689';
+-- DELETE FROM holdings_htitem WHERE volume_id = 'nyp.33433090750278';
+
+
+-- INSERT INTO holdings_htitem VALUES ('nyp.33433081645446', 'allow', 'pd', 530786, 'v. 14 (July - Dec. 1788)', 'NYP', '.b150878291', '1568460', 'bib', '2010-10-03 19:30:31', 'multi', 0, '14', NULL, 0, 1788, 'enk', 'eng', 'SE');
+-- INSERT INTO holdings_htitem VALUES ('inu.32000013031176', 'deny', 'ic', 6056065, '1925', 'INU', '2165200', '8339424', 'bib', '2009-10-29 23:30:10', 'mono', 0, NULL, '1925', 0, 1925, 'fr', 'fre', 'SE');
+-- INSERT INTO holdings_htitem VALUES ('uc1.32106020471998', 'deny', 'ic', 8223069, 'v.3', 'UC', '.b13567536', '150618385', 'bib', '2010-07-11 22:30:25', 'multi', 0, '3', NULL, 0, 1975, 'fr', 'fre', 'BK');
+-- INSERT INTO holdings_htitem VALUES ('nyp.33433023168507', 'allow', 'pdus', 8605042, 'v. 1-2', 'NYP', '.b138638305', '31525047', 'bib', '2010-10-03 19:30:31', 'multi', 0, '1-2', NULL, 0, 1888, 'enk', 'eng', 'BK');
+
+-- INSERT INTO holdings_htitem VALUES ('nyp.33433068275159', 'allow', 'pd', 8634733, 'v. 25-26 (1907-08)', 'NYP', '.b154167575', '49921607', 'bib', '2010-10-03 19:30:31', 'multi', 0, '25-26', '1907-08', 0, 1908, 'ilu', 'eng', 'SE');
+-- INSERT INTO holdings_htitem VALUES ('nyp.33433081678660', 'allow', 'pdus', 8696083, 'v. 29 (1907/08)', 'NYP', '.b149598373', '45584819', 'bib', '2010-10-03 19:30:31', 'multi', 0, '29', '1907/08', 0, 1908, 'xx', 'eng', 'SE');
+-- INSERT INTO holdings_htitem VALUES ('nyp.33433009982327', 'allow', 'pdus', 8701086, '1887 (20 Leg., 2 Anno, pt. 5-6)', 'NYP', '.b153068176', '48671810', 'bib', '2010-10-03 19:30:31', 'multi', 0, '20 Leg. 2 Anno pt.5-6', '1887', 0, 1887, 'bl', 'por', 'SE');
+-- INSERT INTO holdings_htitem VALUES ('coo.31924095682286', 'deny', 'ic', 10853521, 'Handbook 2002:Oct.', 'COO', '4471205', '193805718', 'bib', '2010-10-03 19:30:31', 'multi', 0, 'Handbook', '2002 Oct.', 0, 2002, 'nyu', 'eng', 'SE');
+
+
+-- INSERT INTO holdings_htitem VALUES ('nyp.33433090762398', 'allow', 'pd', 640246, 'v. 26 (July-Dec. 1903)', 'NYP', '.b120317333','1369875', 'bib', '2013-04-19 19:26:50', 'multi', 0, '26', 'July-Dec:1903', 0, 1903, 'nyu', 'eng', 'SE');
+-- INSERT INTO holdings_htitem VALUES ('nyp.33433090822689', 'allow', 'pdus', 12306300, '1888-1892', 'NYP', '.b12364642x','33903612', 'bib', '2013-04-19 19:26:50', 'multi', 0, '', '1888-1892', 0, 1892, 'at', 'eng', 'SE');
+-- INSERT INTO holdings_htitem VALUES ('nyp.33433090750278', 'allow', 'pd', 12307158, 'v. 102 (Apr.-June 1866)', 'NYP', '.b153249730','22091523', 'bib', '2013-04-19 19:26:48', 'multi', 0, '102', 'Apr-June:1866', 0, 1903, 'nyu', 'eng', 'SE');
