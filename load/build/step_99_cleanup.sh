ec6bc580c977275572bf6fceba8e29f3603c146a
Martin Warin <mwarin@umich.edu>
2014-06-10 14:23:28 -0400

    now compressing current aws dir

--- load/build/step_99_cleanup.sh
+++ load/build/step_99_cleanup.sh
@@ -18,3 +18,12 @@ mv -v $LOGDIR $LOGROOT/builds/$todaystr;
 
 # Recreate $LOGROOT/builds/current
 mkdir -p $LOGDIR;
+
+# Targz some bulky files left over:
+## AWS:
+last_aws=`ls $DATADIR/aws/ | tail -1`
+echo "Making a tar.gz of AWS $last_aws"
+## cd to the dir instead of messing with --strip-components.
+cd $DATADIR/aws/$last_aws/;
+tar --remove-files -czvf $last_aws.tar.gz part-*;
+cd $SCRIPTPATH;
