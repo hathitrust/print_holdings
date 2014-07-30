0c95286e95ccffb90a3547645ca52c8a813ce97c
Martin Warin <mwarin@umich.edu>
2014-07-30 14:11:05 -0400

    renamed aws_upload.sh

--- load/build/step_09a.sh
+++ load/build/step_09a.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-
-# Combines scriptable pre-manual substeps of step 9 (the cloud step) in the monthlies.
+# Takes a path to a tab-separated file with format:
+# <member_id><TAB><(mono|multi|serial)>
+# ... and uploads the corresponding files to AWS.
 
 pushd `dirname $0` > /dev/null;
 SCRIPTPATH=`pwd`;
@@ -9,22 +9,13 @@ source $SCRIPTPATH/build_lib.sh;
 
 input_current="s3://umich-lib-phdb-1/input/Current";
 
-date;
-echo "Before:";
-s3cmd ls $input_current/;
+upload_to_aws () {
+    echo member $1 type $2;
+    # Turn on the -n flag for dry-run.
+    s3cmd --no-progress put $HTDIR/HT00*_$1.$2.tsv $input_current/;
+}
 
-# Empty the current input bucket.
-date;
-echo "DELeting old data.";
-s3cmd del $input_current/*;
-
-# Put current HT003 files in current input bucket.
-date;
-echo "PUTting new data.";
-s3cmd --no-progress put $HTDIR/HT003_*.tsv $input_current/;
-
-echo "After:";
-s3cmd ls $input_current/;
-
-date;
-echo "Finished.";
+# Skip commented-out lines. Only get ones with a tab.
+egrep -v '#' $1 | egrep -o $'([^\t]+\t[^\t]+)' | while read -r line ; do
+    upload_to_aws $line
+done
