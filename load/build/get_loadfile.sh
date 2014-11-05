e859e2747613fd585f19daf07b3345a4d6d71176
Martin Warin <mwarin@umich.edu>
2014-11-05 13:07:00 -0500

    changed grep, cosmetically

--- load/build/get_loadfile.sh
+++ load/build/get_loadfile.sh
@@ -1,3 +1,3 @@
 cd /htapps/mwarin.babel/phdb_scripts/data/memberdata; 
-perl ~/useful/tree.pl | grep HT003 | grep -v estimate | grep -v old | sed -r 's/.+(HT003_(.+).(mono|multi|serial).tsv)/\2\t\3/g' | sort;
+perl ~/useful/tree.pl | grep HT003 | egrep -v '\.estimate|old' | sed -r 's/.+(HT003_(.+).(mono|multi|serial).tsv)/\2\t\3/g' | sort;
 cd -;
