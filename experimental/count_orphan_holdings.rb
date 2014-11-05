b2d0a00980a9d94bb7aa76b4fce7913d355f29bf
Martin Warin <mwarin@umich.edu>
2014-11-05 13:08:07 -0500

    removed access stuff

--- experimental/count_orphan_holdings.rb
+++ experimental/count_orphan_holdings.rb
@@ -7,26 +7,25 @@ db = Hathidb::Db.new();
 conn = db.get_conn();
 prefixes = {};
 q = [
-     "SELECT hhhj.volume_id, hf.access",
-     "FROM holdings_htitem_htmember_jn AS hhhj",
-     "LEFT JOIN hathi_files AS hf ON (hf.htid = hhhj.volume_id)",
-     "WHERE hhhj.member_id = ''"
+     "SELECT volume_id",
+     "FROM holdings_htitem_htmember_jn",
+     "WHERE member_id = ''"
     ].join(' ');
 
 conn.query(q) do |row|
   pre,post = *row[:volume_id].split('.');
-  access   = row[:access];
 
   prefixes[pre] ||= {};
   prefixes[pre]['count'] ||= 0;
   prefixes[pre]['count']  += 1;
-  prefixes[pre][access]  ||= 0;
-  prefixes[pre][access]   += 1;
+  prefixes[pre]['guess']  = [];
 end
+
+# See if a member holds other volumes with the same prefix.
 
 # Print sorted by ascending count.
 total = 0;
-puts %w(member count allow deny).join("\t");
+puts %w(member count).join("\t");
 prefixes.keys.sort{|a,b| prefixes[a]['count'] <=> prefixes[b]['count']}.each do |k|
   puts [k, %w(count allow deny).map{|x| prefixes[k][x]}].join("\t");
   total += prefixes[k]['count'];
