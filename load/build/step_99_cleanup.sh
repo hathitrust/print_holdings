d99416447fe99220ada115c594cf7394202c4d49
Martin Warin <mwarin@umich.edu>
2014-11-13 14:00:24 -0500

    changed the way aws output is being targzed

--- load/build/step_99_cleanup.sh
+++ load/build/step_99_cleanup.sh
@@ -8,24 +8,24 @@ source $SCRIPTPATH/build_lib.sh;
 
 ruby $SCRIPTPATH/trunc_old.rb;
 
-# Move all logfiles in the build (if any) to the current build log dir.
+## Move all logfiles in the build (if any) to the current build log dir.
 mv -v $SCRIPTPATH/*.log $LOGDIR;
 
 todaystr=`date +'%Y-%m-%d'`;
 
-# Rename the current build log & data dirs.
+## Rename the current build log & data dirs.
 mv -v $LOGDIR $LOGROOT/builds/$todaystr;
 mv -v $DATADIR/builds/current $DATADIR/builds/$todaystr;
 
-# Recreate $LOGROOT/builds/current
+## Recreate $LOGROOT/builds/current
 mkdir -p $LOGDIR;
 mkdir -p $DATADIR/builds/current;
 
-# Targz some bulky files left over:
+## Targz some bulky files left over:
 ## AWS:
 last_aws=`ls -d1 $DATADIR/aws/*/ | tail -1`
 echo "Making a tar.gz of AWS $last_aws"
 ## cd to the dir instead of messing with --strip-components.
-cd $DATADIR/aws/$last_aws/;
-tar --remove-files -czvf $last_aws.tar.gz part-*;
+cd $last_aws/;
+tar --remove-files -czvf $todaystr.tar.gz part-*;
 cd $SCRIPTPATH;
