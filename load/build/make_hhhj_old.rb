5b6792fc3624377598ca10e736fcf51b047fef8a
Martin Warin <mwarin@umich.edu>
2014-12-12 14:59:40 -0500

    rewrite that uses LOAD DATA LOCAL INFILE instead of INSERT

--- load/build/make_hhhj_old.rb
+++ load/build/make_hhhj_old.rb
@@ -1,4 +1,5 @@
 # Part of step_00_backup.sh
+# Copies the current holdings_htitem_htmember_jn into holdings_htitem_htmember_jn_old.
 
 require 'hathidb';
 require 'hathilog';
@@ -10,14 +11,23 @@ db    = Hathidb::Db.new();
 conn  = db.get_conn();
 table = "holdings_htitem_htmember_jn";
 
-[
- "TRUNCATE TABLE #{table}_old",
- "INSERT INTO #{table}_old (SELECT * FROM #{table})"
-].each do |q|
-  log.d(q);
-  pq = conn.prepare(q);
-  pq.execute();
+delete_sql = "TRUNCATE TABLE holdings_htitem_htmember_jn_old";
+select_sql = "SELECT volume_id, member_id, copy_count, lm_count, wd_count, brt_count access_count FROM holdings_htitem_htmember_jn";
+hdout      = Hathidata::Data.new('holdings_htitem_htmember_jn_old.dat').open('w');
+load_sql   = "LOAD DATA LOCAL INFILE ? INTO TABLE holdings_htitem_htmember_jn_old";
+
+log.d(delete_sql);
+conn.execute(delete_sql);
+
+log.d(select_sql);
+conn.query(select_sql) do |row|
+  hdout.file.puts [:volume_id, :member_id, :copy_count, :lm_count, :wd_count, :brt_count, :access_count].map{|x| row[x]}.join("\t");
 end
+hdout.close();
+
+log.d(load_sql);
+load_q = conn.prepare(load_sql);
+load_q.execute(hdout.path);
 
 conn.close();
 log.d("Finished");
