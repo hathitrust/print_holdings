ac2610fffc28704314295ba85a33a4b53b627a77
Martin Warin <mwarin@umich.edu>
2015-02-18 11:00:02 -0500

    rewrote to be less resource hoggy, but it turned out not to be a problem. better this anyway tho?

--- load/build/populate_holdings_htitem_htmember_jn_dev.rb
+++ load/build/populate_holdings_htitem_htmember_jn_dev.rb
@@ -6,6 +6,7 @@ require 'hathidata';
 
 db   = Hathidb::Db.new();
 conn = db.get_conn();
+conn.java.setAutoReconnect(true); # This does nothing?
 log  = Hathilog::Log.new();
 log.d("Started");
 
@@ -15,32 +16,42 @@ count_sql  = %w[
   UNION 
   SELECT 'holdings_htitem_htmember_jn_dev' AS t, COUNT(*) AS c FROM holdings_htitem_htmember_jn_dev
 ].join(" ");
+count_q = conn.prepare(count_sql);
 
-trunc_sql  = "TRUNCATE holdings_htitem_htmember_jn_dev";
-select_sql = "SELECT #{cols.join(', ')} FROM holdings_htitem_htmember_jn";
+trunc_sql  = "TRUNCATE TABLE holdings_htitem_htmember_jn_dev";
+
+get_members_sql = 'SELECT distinct member_id FROM holdings_htitem_htmember_jn';
+get_members_q   = conn.prepare(get_members_sql);
+
+select_sql = "SELECT #{cols.join(', ')} FROM holdings_htitem_htmember_jn WHERE member_id = ?";
+select_q   = conn.prepare(select_sql);
+
 load_sql   = "LOAD DATA LOCAL INFILE ? INTO TABLE holdings_htitem_htmember_jn_dev";
-hdout      = Hathidata::Data.new('holdings_htitem_htmember_jn.dat').open('w');
+load_q     = conn.prepare(load_sql);
 
 log.d(count_sql);
-conn.query(count_sql) do |row|
+count_q.enumerate() do |row|
   puts "#{row[:t]}\t#{row[:c]}";
 end
 
 log.d(trunc_sql);
 conn.execute(trunc_sql);
 
-log.d(select_sql);
-conn.enumerate(select_sql) do |row|
-  hdout.file.puts(cols.map{ |x| row[x] }.join("\t"));
+hdout = Hathidata::Data.new('holdings_htitem_htmember_jn.dat').open('w');
+get_members_q.enumerate do |mrow|
+  member_id = mrow[:member_id];
+  puts select_sql.sub('?', member_id);
+  select_q.enumerate(member_id) do |row|
+    hdout.file.puts(cols.map{ |x| row[x] }.join("\t"));
+  end
 end
 hdout.close();
 
 log.d(load_sql);
-load_q = conn.prepare(load_sql);
 load_q.execute(hdout.path);
 
 log.d(count_sql);
-conn.query(count_sql) do |row|
+count_q.enumerate() do |row|
   puts "#{row[:t]}\t#{row[:c]}";
 end
 
