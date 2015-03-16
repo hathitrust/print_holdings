70ce0cf5ff87d1e1f9c96b8f2c267743c89bb8f5
Martin Warin <mwarin@umich.edu>
2015-03-16 14:52:24 -0400

    displaying counts by member before and after, not just after

--- load/build/step_10g.rb
+++ load/build/step_10g.rb
@@ -8,17 +8,36 @@ db   = Hathidb::Db.new();
 conn = db.get_conn();
 
 # So we can measure progress.
-def count_unclaimed (conn, log)
+def count_before_after (conn, log)
   q = "SELECT COUNT(*) AS c FROM holdings_htitem_htmember_jn WHERE member_id = ''";
   log.d(q);
   conn.query(q) do |r|
     log.d(r[:c]);
   end
+
+  # List how many by who.
+  count_by_member_before_after = %W<
+    SELECT
+      member_id,
+      COUNT(*) AS c
+    FROM
+      holdings_htitem_htmember_jn
+    WHERE
+      lm_count IS NULL
+    GROUP BY
+      member_id
+  >.join(' ');
+
+  log.d("Volumes with zero lm count, per member:")
+  log.d(count_by_member_before_after);
+  conn.query(count_by_member_before_after) do |row|
+    log.d("#{row[:member_id]}\t#{row[:c]}");
+  end
 end
 
 # Check initial count.
 log.d("Unclaimed count before update:");
-count_unclaimed(conn, log);
+count_before_after(conn, log);
 
 # Map of prefix->member_id, reads like:
 # volume "uma.ark:/13960/t5hb1q086" goes to "umass".
@@ -70,28 +89,7 @@ end
 
 # Check final count.
 log.d("Unclaimed count after update:");
-count_unclaimed(conn, log);
-
-log.d("Volumes with zero lm count, per member:")
-
-# Now list how many by who.
-q2 = %W<
-  SELECT
-    member_id,
-    COUNT(*) AS c
-  FROM
-    holdings_htitem_htmember_jn
-  WHERE
-    lm_count IS NULL
-  GROUP BY
-    member_id
->.join(' ');
-
-log.d(q2);
-
-conn.query(q2) do |row|
-  log.d("#{row[:member_id]}\t#{row[:c]}");
-end
+count_before_after(conn, log);
 
 conn.close();
 log.i("Finished");
