a94d633428e9a1fb9584e8d017fb0499f1a18693
Martin Warin <mwarin@umich.edu>
2015-03-16 15:09:34 -0400

    data got too big, split into one LOAD DATA LOCAL INFILE per member_id

--- load/build/make_hhhj_old.rb
+++ load/build/make_hhhj_old.rb
@@ -13,23 +13,36 @@ conn  = db.get_conn();
 table = "holdings_htitem_htmember_jn";
 
 delete_sql = "TRUNCATE TABLE holdings_htitem_htmember_jn_old";
-select_sql = "SELECT volume_id, member_id, copy_count, lm_count, wd_count, brt_count access_count FROM holdings_htitem_htmember_jn";
-hdout      = Hathidata::Data.new('holdings_htitem_htmember_jn_old.dat').open('w');
-load_sql   = "LOAD DATA LOCAL INFILE ? INTO TABLE holdings_htitem_htmember_jn_old";
-cols       = [:volume_id, :member_id, :copy_count, :lm_count, :wd_count, :brt_count, :access_count];
+
+get_members_sql = "SELECT DISTINCT member_id FROM holdings_htitem_htmember_jn WHERE member_id != ''";
+get_members_q   = conn.prepare(get_members_sql);
+
+select_sql = "SELECT volume_id, member_id, copy_count, lm_count, wd_count, brt_count, access_count FROM holdings_htitem_htmember_jn WHERE member_id = ?";
+select_q   = conn.prepare(select_sql);
+
+load_sql = "LOAD DATA LOCAL INFILE ? INTO TABLE holdings_htitem_htmember_jn_old";
+load_q   = conn.prepare(load_sql);
 
 log.d(delete_sql);
 conn.execute(delete_sql);
 
-log.d(select_sql);
-conn.query(select_sql) do |row|
-  hdout.file.puts cols.map{|x| row[x]}.join("\t");
-end
-hdout.close();
+cols = [:volume_id, :member_id, :copy_count, :lm_count, :wd_count, :brt_count, :access_count];
 
-log.d(load_sql);
-load_q = conn.prepare(load_sql);
-load_q.execute(hdout.path);
+hdout = nil;
+log.d(get_members_sql);
+get_members_q.enumerate() do |member_row|
+  member_id = member_row[:member_id];
+  log.d(select_sql);
+  log.d(member_id);
+  hdout = Hathidata::Data.new('holdings_htitem_htmember_jn_old.dat').open('w');  
+  select_q.enumerate(member_id) do |row|
+    hdout.file.puts(cols.map{|x| row[x]}.join("\t"));
+  end
+  hdout.close();
+  log.d(load_q);
+  load_q.execute(hdout.path);
+end
+hdout.delete() if !hdout.nil?;
 
 conn.close();
 log.d("Finished");
