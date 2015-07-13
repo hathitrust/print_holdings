ca957f01a795fc156ed1d409f13e2ed72313b246
Martin Warin <mwarin@umich.edu>
2015-07-13 11:00:21 -0400

    use the .rb instead

--- load/build/step_10e.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pushd `dirname $0` > /dev/null;
-SCRIPTPATH=`pwd`;
-popd > /dev/null;
-source $SCRIPTPATH/build_lib.sh;
-
-jruby $SCRIPTPATH/add_source_items_to_htitem_htmember_jn.rb $DATADIR/builds/current/deposits.txt;
