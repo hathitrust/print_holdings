4f0825f6d4c610701fb67717ddcba6ae6c02eb0b
Martin Warin <mwarin@umich.edu>
2015-08-06 13:54:44 -0400

    broke apart

--- load/build/step_00_backup.sh
+++ load/build/step_00_backup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# This is the backup step.
+# This is part of the backup step.
 # Rather than making backups here and there during the process, let's
 # do all the backups (that we can) before we start doing anything.
+
+# This script can be nohupped.
 
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd`
@@ -23,16 +25,5 @@ wc -l $HTDIR/HT003_*.tsv > $LOGDIR/ht00x_file_linecounts.txt
 # holdings_htitem_htmember_jn -> holdings_htitem_htmember_jn_old, takes about 2,5 - 3 h.
 echo "$SCRIPTPATH/make_hhhj_old.rb";
 ruby $SCRIPTPATH/make_hhhj_old.rb;
-
-echo "vvv Going to need password here! vvv";
-
-# Back up holdings_htitem_oclc to file.
-bash $SCRIPTPATH/backup_table.sh holdings_htitem_oclc;
-
-# Used to be 12b.
-bash $SCRIPTPATH/backup_table.sh holdings_H_counts
-
-# Back up holdings_htitem_htmember_jn to file.
-bash $SCRIPTPATH/backup_table.sh holdings_htitem_htmember_jn;
 
 echo "$0 done.";
---
+++ load/build/step_00_backup_interactive.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This is part of the backup step.
+# This part cannot be nohupped.
+
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+source $SCRIPTPATH/build_lib.sh;
+
+echo "Running step_00_backup_interactive.sh" >> $LOGDIR/captain.log;
+
+echo "vvv Going to need password here! vvv";
+
+# Back up holdings_htitem_oclc to file.
+bash $SCRIPTPATH/backup_table.sh holdings_htitem_oclc;
+
+# Used to be 12b.
+bash $SCRIPTPATH/backup_table.sh holdings_H_counts
+
+# Back up holdings_htitem_htmember_jn to file.
+bash $SCRIPTPATH/backup_table.sh holdings_htitem_htmember_jn;
+
+echo "$0 done.";
