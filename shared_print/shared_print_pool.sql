0f2321c162f2450181319876a3969bb4ae1933a2
Martin Warin <mwarin@umich.edu>
2016-10-18 10:10:10 -0400

    added local_h and local_oclc

--- shared_print/shared_print_pool.sql
+++ shared_print/shared_print_pool.sql
@@ -1,14 +1,15 @@
-CREATE VIEW v_shared_print_pool AS
-SELECT hm.member_id, hm.status, hm.item_condition, COALESCE(o.oclc_x, hm.oclc) AS oclc
-FROM holdings_memberitem AS hm LEFT JOIN oclc_resolution AS o ON (hm.oclc = o.oclc_y)
-WHERE hm.item_type = 'mono';
-
+-- oclc is the resolved oclc number, it has been looked up in oclc_resolution
+-- local_oclc is the oclc number the member uses
 CREATE TABLE shared_print_pool (
     id INT NOT NULL AUTO_INCREMENT,
     holdings_memberitem_id INT NOT NULL,    
     member_id VARCHAR(20) NOT NULL,
     oclc bigint(20) NOT NULL,
+    local_oclc bigint(20) NOT NULL,
+    local_h SMALLINT NULL DEFAULT NULL,
     PRIMARY KEY (id),
     INDEX shared_print_pool_oclc (oclc),
-    INDEX shared_print_pool_member_id (member_id)    
+    INDEX shared_print_pool_local_oclc (local_oclc),
+    INDEX shared_print_pool_member_id (member_id),
+    INDEX shared_print_pool_local_h (local_h)
 );
