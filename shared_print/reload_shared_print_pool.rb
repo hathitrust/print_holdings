0913aa448f53f5816eedd7c606bb198687c99b07
Martin Warin <mwarin@umich.edu>
2016-10-18 10:09:00 -0400

    no longer caring about govdoc, added local_h and local_oclc

--- shared_print/reload_shared_print_pool.rb
+++ shared_print/reload_shared_print_pool.rb
@@ -1,32 +1,74 @@
+require 'hathidata';
 require 'hathidb';
 require 'hathilog';
 
-# shared_print_members = %w[umich wisc yale stanford iastate utexas columbia];
-shared_print_members = %w[tufts yale ucdavis];
+log = Hathilog::Log.new();
+
+test = false;
+if ARGV.include?('test') then
+  test = true;
+end
+
+shared_print_members = [];
+if test == true then
+  shared_print_members = %w[cornell duke emory];
+else
+  Hathidata.read('shared_print_members.tsv') do |line|
+    member_id = line.strip;
+    shared_print_members << member_id;
+  end
+end
 
 db   = Hathidb::Db.new();
 conn = db.get_conn();
 
-log = Hathilog::Log.new();
-
 trunc_sql  = "TRUNCATE TABLE shared_print_pool";
+# Now allowing govdocs as well.
+#     AND (hm.gov_doc IS NULL OR hm.gov_doc = '0')
 insert_sql = %w{
-    INSERT INTO shared_print_pool (holdings_memberitem_id, member_id, oclc) 
-    SELECT hm.id, hm.member_id, COALESCE(o.oclc_x, hm.oclc) AS oclc 
-    FROM holdings_memberitem AS hm 
-    LEFT JOIN oclc_resolution AS o ON (hm.oclc = o.oclc_y) 
+    INSERT INTO shared_print_pool (holdings_memberitem_id, member_id, oclc, local_oclc)
+    SELECT hm.id, hm.member_id, COALESCE(o.oclc_x, hm.oclc) AS oclc, hm.oclc AS local_oclc
+    FROM holdings_memberitem AS hm
+    LEFT JOIN oclc_resolution AS o ON (hm.oclc = o.oclc_y)
+    JOIN holdings_cluster_oclc AS hco ON (hco.oclc = COALESCE(o.oclc_x, hm.oclc))
     WHERE hm.item_type = 'mono'
-    AND (hm.gov_doc IS NULL OR hm.gov_doc = '0')
     AND hm.member_id = ?
 }.join(' ');
 
+get_local_h_sql = %w{
+    SELECT oclc, COUNT(DISTINCT member_id) AS h
+    FROM shared_print_pool
+    GROUP BY oclc
+    ORDER BY oclc
+}.join(' ');
+
+update_local_h_sql = 'UPDATE shared_print_pool SET local_h = ? WHERE oclc = ?';
+
+# Empty pool.
 log.d(trunc_sql);
 conn.execute(trunc_sql);
 
+# Populate pool.
 insert_q = conn.prepare(insert_sql);
 shared_print_members.each do |member_id|
   log.d(insert_sql.sub('?', "'#{member_id}'"));
   insert_q.execute(member_id);
+end
+
+# Calculate local_h (H relative to pool)
+get_local_h_q    = conn.prepare(get_local_h_sql);
+update_local_h_q = conn.prepare(update_local_h_sql);
+
+log.d("Updating local_h");
+i = 0;
+get_local_h_q.enumerate() do |row|
+  i += 1;
+  if i % 10000 == 0 then
+    log.d(i);
+  end
+  oclc = row[:oclc];
+  h    = row[:h];
+  update_local_h_q.execute(h, oclc);
 end
 
 log.d("Done");
