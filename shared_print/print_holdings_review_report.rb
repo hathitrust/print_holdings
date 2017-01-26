d2478ff9901e382eee5998fb28caa5246d76b333
Martin Warin <mwarin@umich.edu>
2017-01-26 14:52:12 -0500

    changed column names

--- shared_print/print_holdings_review_report.rb
+++ shared_print/print_holdings_review_report.rb
@@ -10,7 +10,8 @@ outfn      = "reports/shared_print/print_holdings_review_#{ids_str}_$ymd.tsv";
 hdout      = Hathidata::Data.new(outfn).open('w');
 group      = false;
 
-# Predefined groups that may be given as input
+# Predefined groups that may be given as input.
+# Note that some groups contain members that aren't (yet?) in SP. They will be ignored.
 groups = {
   'foo'     => %w[brynmawr haverford],
   'ivyplus' => %w[brown columbia cornell dartmouth duke harvard jhu mit princeton stanford uchicago upenn yale],
@@ -34,11 +35,11 @@ report_sql = %W{
   spp.gov_doc,
   spp.item_condition,
   spp.local_oclc,
-  spp.oclc,
-  hhh.H,
-  spp.local_h
+  spp.oclc AS resolved_oclc,
+  hhh.H AS overlap_ht,
+  spp.local_h AS overlap_sp
   FROM shared_print_pool          AS spp
-  JOIN holdings_cluster_oclc      AS hco  ON (spp.oclc = hco.oclc)
+  JOIN holdings_cluster_oclc      AS hco  ON (spp.oclc       = hco.oclc)
   JOIN holdings_cluster           AS hc   ON (hco.cluster_id = hc.cluster_id)
   JOIN holdings_cluster_htitem_jn AS hchj ON (hc.cluster_id  = hchj.cluster_id)
   JOIN holdings_htitem_H          AS hhh  ON (hchj.volume_id = hhh.volume_id)
@@ -48,49 +49,51 @@ report_sql = %W{
   ORDER BY spp.oclc, spp.member_id
 }.join(' ');
 
-# If doing a group report, use this to get group_h
-group_h_sql = %W{
-  SELECT COUNT(DISTINCT member_id) AS group_h
+# If doing a group report, use this to get overlap_group
+overlap_group_sql = %W{
+  SELECT COUNT(DISTINCT member_id) AS overlap_group
   FROM shared_print_pool 
   WHERE oclc = ? AND member_id IN (#{qmarks})
 }.join(' ');
 
-header = %w{member_id local_id gov_doc item_condition local_oclc oclc h local_h};
+header = %w{member_id local_id gov_doc item_condition local_oclc resolved_oclc overlap_ht overlap_sp};
 legend = [
-  "local_oclc : the OCLC number used by #{member_ids.join(',')} in their latest print holdings submission",
-  "oclc : resolved OCLC number",
-  "h : number of HathiTrust members (including Shared Print members) holding the same OCLC number",
-  "local_h : number of Shared Print members (including #{member_ids.join(',')}) holding the same OCLC number",
+  "local_oclc : the OCLC number used in the latest print holdings submission",
+  "resolved_oclc : resolved OCLC number",
+  "overlap_ht : number of HathiTrust members (including Shared Print members) holding the same resolved_oclc number",
+  "overlap_sp : number of Shared Print members holding the same resolved_oclc number",
 ];
 
 # Tweak header and legend if doing a group report.
 if group then
-  legend << "group_h : number of members within #{ids_str} holding the same OCLC number";
-  header << 'group_h';
+  legend << "overlap_group : number of members within #{ids_str} holding the same resolved_oclc number";
+  header << 'overlap_group';
 end
 
 legend.each do |leg|
-  hdout.file.puts("#\t#{leg}");
+  hdout.file.puts("# #{leg}");
 end
 
 hdout.file.puts(header.join("\t"));
 
-report_q = conn.prepare(report_sql);
-group_h_q = conn.prepare(group_h_sql);
+report_q  = conn.prepare(report_sql);
+overlap_group_q = conn.prepare(overlap_group_sql);
 memo_oclc = nil;
 memo_h    = nil;
 
 report_q.enumerate(*member_ids) do |row|
   vals = row.to_a;
+  # Group needs overlap_group
   if group then
     # Poor mans memoization
-    if row[:oclc] == memo_oclc then
+    if row[:resolved_oclc] == memo_oclc then
       vals << memo_h;
     else
-      group_h_q.enumerate(row[:oclc], *member_ids) do |h_row|
-        vals << h_row[:group_h];
-        memo_oclc = row[:oclc];
-        memo_h = h_row[:group_h];
+      # Look up overlap_group for oclc if memo failed.
+      overlap_group_q.enumerate(row[:resolved_oclc], *member_ids) do |h_row|
+        vals << h_row[:overlap_group];
+        memo_oclc = row[:resolved_oclc];
+        memo_h    = h_row[:overlap_group];
       end
     end
   end
