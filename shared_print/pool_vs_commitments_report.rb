b841da584f6b51c2ad9f0947e4b5f4666721189c
Martin Warin <mwarin@umich.edu>
2017-09-26 10:41:34 -0400

    sql fixes

--- shared_print/pool_vs_commitments_report.rb
+++ shared_print/pool_vs_commitments_report.rb
@@ -7,30 +7,43 @@ require 'hathidata';
 
 db   = Hathidb::Db.new();
 conn = db.get_conn();
-get_holdings_count        = conn.prepare("SELECT COUNT(*)       AS c FROM holdings_memberitem WHERE member_id = ? AND item_type = 'mono'");
-get_pool_count            = conn.prepare("SELECT COUNT(*)       AS c FROM shared_print_pool WHERE member_id = ?");
+get_holdings_count        = conn.prepare("SELECT COUNT(*)       AS c FROM holdings_memberitem      WHERE member_id = ? AND item_type = 'mono'");
+get_pool_count            = conn.prepare("SELECT COUNT(*)       AS c FROM shared_print_pool        WHERE member_id = ?");
 get_commitment_count      = conn.prepare("SELECT COUNT(*)       AS c FROM shared_print_commitments WHERE member_id = ?");
-get_unique_in_pool_count  = conn.prepare("SELECT COUNT(local_h) AS c FROM shared_print_pool WHERE member_id = ? AND local_h = 1");
+get_unique_in_pool_count  = conn.prepare("SELECT COUNT(local_h) AS c FROM shared_print_pool        WHERE member_id = ? AND local_h = 1");
 get_unique_in_hathi_count = conn.prepare(%w{
     SELECT COUNT(hhh.H) AS c
     FROM shared_print_pool          AS spp
     JOIN holdings_cluster_oclc      AS hco  ON (spp.resolved_oclc = hco.oclc)
-    JOIN holdings_cluster           AS hc   ON (hco.cluster_id = hc.cluster_id)
-    JOIN holdings_cluster_htitem_jn AS hchj ON (hc.cluster_id  = hchj.cluster_id)
-    JOIN holdings_htitem_H          AS hhh  ON (hchj.volume_id = hhh.volume_id)
+    JOIN holdings_cluster           AS hc   ON (hco.cluster_id    = hc.cluster_id)
+    JOIN holdings_cluster_htitem_jn AS hchj ON (hc.cluster_id     = hchj.cluster_id)
+    JOIN holdings_htitem_H          AS hhh  ON (hchj.volume_id    = hhh.volume_id)
     WHERE spp.member_id = ?
     AND hc.cluster_type = 'spm'
     AND hhh.H = 1
 }.join(' '));
+get_unique_commitments = conn.prepare(%w{
+    SELECT COUNT(*) AS c FROM (
+        SELECT t1.resolved_oclc, COUNT(t2.resolved_oclc) AS h 
+        FROM shared_print_commitments AS t1 
+        JOIN shared_print_commitments AS t2 ON (t1.resolved_oclc = t2.resolved_oclc) 
+        WHERE t1.member_id = ? 
+        GROUP BY t1.resolved_oclc HAVING h = 1
+    ) AS x
+}.join(' '));
 
 hdout = Hathidata::Data.new("reports/shared_print/overview_counts_$ymd.tsv").open('w');
-hdout.file.puts(%w[member_id holdings_count pool_count commitment_count percent_pool_committed unique_in_pool unique_in_hathi].join("\t"));
+hdout.file.puts(
+  %w[member_id holdings_count pool_count commitment_count percent_pool_committed 
+     unique_in_pool unique_in_hathi unique_commitments].join("\t")
+);
 
 total_holdings_count        = 0;
 total_pool_count            = 0;
 total_commitment_count      = 0;
 total_unique_in_pool_count  = 0;
 total_unique_in_hathi_count = 0;
+total_unique_commitments    = 0;
 
 Hathidata.read("shared_print_members.tsv") do |member_id|
   member_id.strip!;
@@ -39,7 +52,8 @@ Hathidata.read("shared_print_members.tsv") do |member_id|
   commitment_count      = 0;
   unique_in_pool_count  = 0;
   unique_in_hathi_count = 0;
-
+  unique_commitments    = 0;
+  
   get_holdings_count.query(member_id) do |row|
     holdings_count = row[:c].to_i;
   end  
@@ -55,20 +69,27 @@ Hathidata.read("shared_print_members.tsv") do |member_id|
   get_unique_in_hathi_count.query(member_id) do |row|
     unique_in_hathi_count = row[:c].to_i;
   end
-
+  get_unique_commitments.query(member_id) do |row|
+    unique_commitments = row[:c].to_i;
+  end
+  
   total_holdings_count        += holdings_count;
   total_pool_count            += pool_count;
   total_commitment_count      += commitment_count;
   total_unique_in_pool_count  += unique_in_pool_count;
   total_unique_in_hathi_count += unique_in_hathi_count;
+  total_unique_commitments    += unique_commitments;
   percent_pool_committed = 'N/A';
 
   if pool_count > 0 && commitment_count > 0 then
     percent_pool_committed = (100 * (commitment_count.to_f / pool_count)).round(2);
-    percent_pool_committed = "#{percent}%"
+    percent_pool_committed = "#{percent_pool_committed}%";
   end
 
-  hdout.file.puts([member_id, holdings_count, pool_count, commitment_count, percent_pool_committed, unique_in_pool_count, unique_in_hathi_count].join("\t"));
+  hdout.file.puts(
+    [member_id, holdings_count, pool_count, commitment_count, percent_pool_committed,
+     unique_in_pool_count, unique_in_hathi_count, unique_commitments].join("\t")
+  );
 end
 
 total_percent_pool_committed = 'N/A';
@@ -77,5 +98,8 @@ if total_pool_count > 0 && total_commitment_count > 0 then
   total_percent_pool_committed = "#{total_percent_pool_committed}%";
 end
 
-hdout.file.puts(["TOTAL", total_holdings_count, total_pool_count, total_commitment_count, total_percent_pool_committed, total_unique_in_pool_count, total_unique_in_hathi_count].join("\t"));
+hdout.file.puts(
+  ["TOTAL", total_holdings_count, total_pool_count, total_commitment_count, total_percent_pool_committed,
+   total_unique_in_pool_count, total_unique_in_hathi_count, total_unique_commitments
+  ].join("\t"));
 hdout.close();
