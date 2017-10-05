eef046d1ddf1ffaacbc53594b071bca9ae332123
Martin Warin <mwarin@umich.edu>
2017-10-05 13:41:43 -0400

    more concise default setting/overriding

--- lib/uniq_oclc.pl
+++ lib/uniq_oclc.pl
@@ -25,32 +25,23 @@ Alter default behavior with:
   --column_delim
   --data_delim
   --oclc_column
+  --verbose
 
 =cut
 
-# Defaults:
-my $column_delim = "\t";
-my $data_delim   = ",";
-my $oclc_column  = 0;
-my $verbose      = 0;
+my $default = {
+    column_delim => "\t",
+    data_delim   => ",",
+    oclc_column  => 0,
+    verbose      => 0,
+};
 
-# Override defaults with --option_name:
-my ($column_delim_input) = map {/^--column_delim=(.+)/ ? $1 : ()} @ARGV;
-$column_delim = $column_delim_input if $column_delim_input;
-
-my ($data_delim_input) = map {/^--data_delim=(.+)/ ? $1 : ()} @ARGV;
-$data_delim = $data_delim_input if $data_delim_input;
-
-my ($oclc_column_input) = map {/^--oclc_column=(.+)/ ? $1 : ()} @ARGV;
-$oclc_column = $oclc_column_input if $oclc_column_input;
-
-my $verbose_input = map {/^--verbose/ ? $1 : ()} @ARGV;
-$verbose = $verbose_input if $verbose_input;
-
-print STDERR "column_delim:[$column_delim]\n";
-print STDERR "data_delim:[$data_delim]\n";
-print STDERR "oclc_column:[$oclc_column]\n";
-print STDERR "verbose:[$verbose]\n";
+foreach my $k (keys %$default) {
+    # Look for default overrides in @ARGV and set accordingly.
+    my ($input) = map {/^--$k=(.+)/ ? $1 : ()} @ARGV;
+    $default->{$k} = $input if $input;
+    print "$k:[$default->{$k}]\n";
+}
 
 # Filter out --option_name from ARGV.
 @ARGV = grep {$_ !~ /^--/} @ARGV;
@@ -61,8 +52,8 @@ while (<>) {
     my $old_line = $line;
     $line =~ s/\"//g;
     # Split line into columns
-    my @columns = split($column_delim, $line);
-    my @ocns    = split($data_delim, $columns[$oclc_column]);
+    my @columns = split($default->{column_delim}, $line);
+    my @ocns    = split($default->{data_delim}, $columns[$default->{oclc_column}]);
     my %uniq    = ();
     OCN_LOOP: foreach my $ocn (@ocns) {
 	# Only deal with columns that loosely resemble oclc numbers.
@@ -72,7 +63,7 @@ while (<>) {
 	if ($ocn =~ m/oc.+(\d+)/i) {
 	    $number = $1;
 	} else {
-	    print STDERR "Rejected $ocn\n" if $verbose;
+	    print STDERR "Rejected $ocn\n" if $default->{verbose};
 	    next OCN_LOOP;
 	}
 	# strip leading zeroes.
@@ -82,10 +73,10 @@ while (<>) {
 	$uniq{$number} = $ocn;
     }
     # Return one surface representation per number in %uniq.
-    $columns[$oclc_column] = join($data_delim, sort values %uniq);
+    $columns[$default->{oclc_column}] = join($default->{data_delim}, sort values %uniq);
     # Put line back together
-    my $new_line           = join($column_delim, @columns);
-    if ($verbose && $old_line ne $new_line) {
+    my $new_line           = join($default->{column_delim}, @columns);
+    if ($default->{verbose} && $old_line ne $new_line) {
 	print STDERR "-$old_line\n";
 	print STDERR "+$new_line\n";
     }
