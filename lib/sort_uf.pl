1b1d9117af6157586c2c7808d9662e1ed28033a6
Martin Warin <mwarin@umich.edu>
2017-11-30 15:16:29 -0500

    almost complete rewrite

--- lib/sort_uf.pl
+++ lib/sort_uf.pl
@@ -3,42 +3,55 @@ use warnings;
 
 =pod
 
-Assumes a tab-separated file as 1st input.
-Accepts a column number (0-indexed) as 2nd input, defaults to 0 if absent.
-Outputs a uniq:ed file, keyed on the given column.
-It's like you could specify the uniq-key for sort -u, essentially.
+Given a file as input, this script will output a file uniqued on the zeroth column.
+Change the column with -f=<column_number>.
+Change the column separator (default "\t") with -F=<column_separator>.
 
 Example: cols.tsv contains
-a       1
-a       2
-b       1
-b       3
+a	1
+a	2
+b	1
+b	3
 
-$ perl sort_uf.pl cols.tsv 0
-a       2
-b       3
+$ perl sort_uf.pl cols.tsv -f=0
+a	2
+b	3
 
-$ perl sort_uf.pl cols.tsv 1
-b       1
-a       2
-b       3
+$ perl sort_uf.pl cols.tsv -f=1
+b	1
+a	2
+b	3
 
 =cut
 
-my $f = shift || die "Need file as 1st input\n";
-my $c = shift || 0;
+my %flags = (
+    'f' => 0,	 # which field to key on
+    'F' => "\t", # field separator
+);
+
+
+my $i = 0;
+# Separate files from flags.
+foreach my $arg (@ARGV) {
+    if ($arg =~ m/^-(.+?)=(\S+)/) {
+	my ($command, $value) = ($1, $2);
+	$flags{$command} = $value;
+	delete $ARGV[$i];
+    } else {
+	$i++;
+    }
+}
+
 my %uniq;
-open (F, $f) || die "Cannot open file $f\n";
-# Read file line by line.
-while (<F>) {
+# Read input line by line.
+while (<>) {
     my $line = $_;
     chomp $line;
     # Split line into cols.
-    my @cols = split("\t", $line);
-    # Store line keyed on column $c if if exists.
-    $uniq{$cols[$c]} = $line if defined $cols[$c];
+    my @cols = split($flags{'F'}, $line);
+    # Store line keyed on column -f if if exists.
+    $uniq{$cols[$flags{'f'}]} = $line if defined $cols[$flags{'f'}];
 }
-close(F);
 
 # Output lines sorted by key.
 foreach my $k (sort keys %uniq) {
