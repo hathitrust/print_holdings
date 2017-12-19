2cb8fb2e1cbc25eb14cd56d678e448e1c6ffc4aa
Martin Warin <mwarin@umich.edu>
2017-12-19 15:04:19 -0500

    added add for ucm purposes

--- lib/active_members.rb
+++ lib/active_members.rb
@@ -7,3 +7,12 @@ conn.query(Hathiquery.get_active_members) do |row|
   puts row[:member_id];
 end
 conn.close();
+
+# For if you want to add a non-active member as an active member.
+# WHY ON EARTH? Because ucm. I break other things if I set ucm to
+# be active, so I'd rather do this.
+ARGV.each do |arg|
+  if arg =~ /add=(\w+)/ then
+    puts $1;
+  end
+end
