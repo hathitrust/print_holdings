5bbfda15b26dc3411f70f7124593fa73087eb42a
Martin Warin <mwarin@umich.edu>
2018-02-16 15:37:45 -0500

    added status=C for dup copy, error handling

--- shared_print/deprecate_records.rb
+++ shared_print/deprecate_records.rb
@@ -23,12 +23,20 @@ trump      994682109   tr.12346  L
 trump      994682109   tr.12347  L
 
 The value for new_status can be: L, D or E.
-L=lost, D=damaged, E=committed in error
+L=Lost, D=Damaged, E=committed in Error, C=duplicate Copy
 
 Look up matching records in shared_print_commitments and move them over
 to shared_print_deprecated and add the status.
 
 =end
+
+class NoDupError < StandardError
+
+end
+
+class BadStatusError < StandardError
+
+end
 
 db   = Hathidb::Db.new();
 conn = db.get_conn();
@@ -52,11 +60,18 @@ insert_sql = %w<
 
 delete_sql = "DELETE FROM shared_print_commitments WHERE id = ?";
 
+check_dup_sql = %w{
+  SELECT COUNT(DISTINCT local_id) AS c 
+  FROM shared_print_commitments
+  WHERE member_id=? AND local_oclc=? AND resolved_oclc=?
+}.join(' ');
+
 # Read 1st row and make sure it matches spec.
 # resolved_oclc is optional.
 header_spec = %w{member_id local_oclc resolved_oclc local_id new_status};
 spec_ok     = false;
 header      = hdin.file.lines.first.strip;
+allowed_status = %w{L D E C};
 
 if header == header_spec.join("\t") then
   spec_ok = true;
@@ -66,49 +81,77 @@ else
     spec_ok = true;
     # Remove resolved_oclc from select
     select_sql.sub!(/AND resolved_oclc=\?/, "");
+    check_dup_sql.sub!(/AND resolved_oclc=\?/, "");
   end
 end
 if !spec_ok then
-  raise "Infile #{infn} header (#{header}) did not match spec."
+  raise ArgumentError.new(
+          "Infile #{infn} header (#{header}) did not match spec."
+        );
 end
 
 # Set up logfile.
 member_id = header.split("\t").first;
-log  = Hathilog::Log.new({:file_name => "#{member_id}_sp_status_update_$ymd.log"});
+# log  = Hathilog::Log.new({:file_name => "#{member_id}_sp_status_update_$ymd.log"});
+log  = Hathilog::Log.new();
 log.i("Updating shared print status for #{member_id}, using #{infn}");
 
 # Prep queries.
 select_q = conn.prepare(select_sql);
 insert_q = conn.prepare(insert_sql);
 delete_q = conn.prepare(delete_sql);
+dups_q   = conn.prepare(check_dup_sql)
 
 i = 1;
+
 hdin.file.each_line do |line|
   line.strip!;
-  i+=1;
-  
-  # Select rows in shared_print_commitments
-  cols       = line.split("\t");
-  new_status = cols.pop();
-  match_rows = [];
-  select_q.enumerate(*cols) do |row|
-    row_h = row.to_h;
-    row_h["deprecation_status"] = new_status;
-    i.log(row_h);
-    match_rows << row_h;
-  end
-
-  if match_rows.keys.size == 0 then
-    log.i("No match on line #{i}:#{line}");
-  else  
-    match_rows.each do |h|
-      # Copy records over to shared_print_deprecated
-      log.i("Inserting #{h} into shared_print_deprecated");
-      insert_q.execute(*h.values);
-      # Delete them from shared_print_commitments
-      log.i("Deleting #{h} from shared_print_commitments");
-      delete_q.execute(h["id"]);
+  i+=1;  
+  begin # Select rows in shared_print_commitments
+    cols       = line.split("\t");
+    new_status = cols.pop();
+    # Only allow allowed statuses.
+    if !allowed_status.include?(new_status) then
+      raise BadStatusError.new(
+              "Status #{new_status} is not allowed, line #{i}:#{line}"
+            );
     end
+    # If C=duplicate Copy, make sure there are duplicates before you deprecate.
+    if new_status == 'C' then
+      # cols[0,3] = [member_id, local_oclc, (resolved_oclc)]
+      dups.enumerate(*cols[0,3]) do |row|
+        dup_count = row[:c].to_i;
+        if dup_count < 2 then
+          raise NoDupError.new(
+                  "Status is C=duplicate Copy, but #{dup_count} copy/ies found. On input line #{i}\n#{line}";
+                );
+        end
+      end
+    end
+    match_rows = [];
+    select_q.enumerate(*cols) do |row|
+      row_h = row.to_h;
+      row_h["deprecation_status"] = new_status;
+      i.log(row_h);
+      match_rows << row_h;
+    end
+
+    if match_rows.size == 0 then
+      log.i("No match on line #{i}:#{line}");
+    else  
+      match_rows.each do |h|
+        # Copy records over to shared_print_deprecated
+        log.i("Inserting #{h} into shared_print_deprecated");
+        insert_q.execute(*h.values);
+        # Delete them from shared_print_commitments
+        log.i("Deleting #{h} from shared_print_commitments");
+        delete_q.execute(h["id"]);
+      end
+    end
+  rescue BadStatusError => bse
+    log.i(bse.message);
+  rescue NoDupError => nde
+    log.i(nde.message);
   end
 end
 
