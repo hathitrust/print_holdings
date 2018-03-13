d97ab9615ae4751693a2d898e01a002e1cfaa373
Martin Warin <mwarin@umich.edu>
2018-03-13 14:21:01 -0400

    added status=C

--- shared_print/deprecated_commitments.sql
+++ shared_print/deprecated_commitments.sql
@@ -1,4 +1,4 @@
 create table shared_print_deprecated like shared_print_commitments;
 alter table shared_print_deprecated add column deprecation_date timestamp default now() not null;
--- Enum values: L=lost, D=damaged, E=committed in error
-alter table shared_print_deprecated add column deprecation_status enum('L', 'D', 'E') not null; 
+-- Enum values: C=Copy,  D=damaged, E=committed in error, L=lost
+alter table shared_print_deprecated add column deprecation_status enum('C','D','E','L') not null; 
