6c07439c1a2c8101f485838d62b9b2a9ff158244
Martin Warin <mwarin@umich.edu>
2018-03-14 14:53:20 -0400

    using values from conf file

--- load/build/backup_table.sh
+++ load/build/backup_table.sh
@@ -3,20 +3,25 @@
 # Makes a gzipped backup of a given table in host mysql-htprep schema ht_repository.
 # Table 'foo' will be saved as "$BK_LOCATION/foo_YMD.sql.gz".
 
-BK_LOCATION=/htapps/mwarin.babel/phdb_scripts/data/sql;
+# Get abs path to this dir.
+pushd `dirname $0` > /dev/null;
+SCRIPTPATH=`pwd`;
+popd > /dev/null;
+source $SCRIPTPATH/build_lib.sh;
+
+BK_LOCATION=$DATADIR/sql;
 
 if [ $1 ]; then
     tablename=$1;
     todaydate=`date +'%Y-%m-%d'`;
     backupfile="backup_${tablename}_${todaydate}.sql.gz";
     user=`whoami`;
-    host='mysql-htprep';
-    schema='ht_repository';
 
     # You will be prompted for password at this point.
-    command="mysqldump --quick --dump-date -h $host -u $user -p $schema $tablename";
+    command="mysqldump --quick --dump-date -h $db_host -u $user -p $db_name $tablename";
     echo "$command > $BK_LOCATION/$backupfile";
     $command | gzip > $BK_LOCATION/$backupfile;
+    echo "Wrote $BK_LOCATION/$backupfile";
 else
     echo "Need tablename as arg.";
 fi
