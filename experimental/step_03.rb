fda01191ad36069fe4fd6c4ce3af0bafdbec053b
Martin Warin <mwarin@umich.edu>
2018-03-27 16:32:03 -0400

    added check for available memory

--- experimental/step_03.rb
+++ experimental/step_03.rb
@@ -1,27 +1,32 @@
-require 'hathidata';
-require 'hathidb';
-require 'hathilog';
+require 'hathidata'
+require 'hathidb'
+require 'hathilog'
+
+# todos:
+# rename queries
+# prepare queries
+
+# first merge_clusters happens at viter=1780134
 
 def setup
-  @log = Hathilog::Log.new();
-  @log.i("Started");
+  @log = Hathilog::Log.new()
+  @log.i("Started")
 
-  db     = Hathidb::Db.new();
-  @conn  = db.get_conn();
+  db     = Hathidb::Db.new()
+  @conn  = db.get_conn()
   @outfn = Hathidata::Data.new('builds/current/cluster_oclc.data_rb').open('w')
 
-  @volid_cluster_d = {};
-  @cluster_volid_d = {};
-  @cluster_oclc_d  = {};
-  @oclc_cluster_d  = {};
+  @cluster_oclc_d = {}
+  @oclc_cluster_d = {}
 end
 
 def teardown
-  @conn.close();
-  @outfn.close();
-  @log.i("Finished");
+  @conn.close()
+  @outfn.close()
+  @log.i("Finished")
 end
 
+# runs query, returns array of items
 def run_list_query(query)
   items = []
   @conn.enumerate(query) do |row|
@@ -39,14 +44,13 @@ end
 
 # creates a new cluster and populates tables appropriately
 def create_cluster(ocns, vol_id)
-  
   # insert into cluster, get id
   ocn0 = ocns[0]
   query2 = "INSERT INTO holdings_cluster (cost_rights_id, osici, last_mod) \
             VALUES (0, '#{ocn0}', SYSDATE())"
   @conn.execute(query2)
-  pkid = run_single_query("SELECT LAST_INSERT_ID()") 
- 
+  pkid = run_single_query("SELECT LAST_INSERT_ID()")
+
   # insert OCNs into cluster_oclc tables
   ocns.each do |nocn|
     oc = nocn.to_i
@@ -59,7 +63,7 @@ def create_cluster(ocns, vol_id)
       puts "this never happens"
     else
       @oclc_cluster_d[oc] = pkid
-    end 
+    end
   end
 
   # insert volume_id into cluster_htitem_jn
@@ -69,18 +73,52 @@ def create_cluster(ocns, vol_id)
   return pkid
 end
 
-def merge_clusters
+# Merges clusters together.  Uses c1's id, adds
+# c2 OCNs and volume_ids to c1, resolves rights, deletes c2 entries from
+# tables.
+def merge_clusters (cid1, cid2)
+  @log.d("Merging cluster id #{cid1}  (ocns #{@cluster_oclc_d[cid1].join(',')})")
+  @log.d("... with cluster id #{cid2} (ocns #{@cluster_oclc_d[cid2].join(',')})")
 
+  queryc1a = "SELECT volume_id FROM holdings_cluster_htitem_jn WHERE cluster_id = '#{cid1}'"
+  queryc2a = "SELECT volume_id FROM holdings_cluster_htitem_jn WHERE cluster_id = '#{cid2}'"
+  c1vids = run_list_query(queryc1a)
+  c2vids = run_list_query(queryc2a)
+
+  c2vids.each do |vid|
+    unless c1vids.include?(vid) then
+      mcquery2 = "INSERT INTO holdings_cluster_htitem_jn (cluster_id, volume_id) \
+                           VALUES ('#{cid1}', '#{vid}')"
+
+      @log.i(mcquery2)
+      @conn.execute(mcquery2)
+    end
+  end
+
+  # insert c2 OCNs into c1
+  c2ocns = @cluster_oclc_d[cid2]
+  c2ocns.each do |ocn|
+    @cluster_oclc_d[cid1] << ocn
+    @oclc_cluster_d[ocn] = cid1
+  end
+  @cluster_oclc_d[cid1].uniq!
+  @cluster_oclc_d.delete(cid2)
+
+  mcquery5a = "DELETE FROM holdings_cluster_htitem_jn WHERE cluster_id = '#{cid2}'"
+  mcquery5c = "DELETE FROM holdings_cluster WHERE cluster_id = '#{cid2}'"
+
+  @conn.execute(mcquery5a)
+  @conn.execute(mcquery5c)  
 end
 
 # Get number of records in each table, then truncate
 def truncate_tables
-  tables = ['holdings_cluster', 'holdings_cluster_oclc', 'holdings_cluster_htitem_jn']
-  tables.each do | t | 
+  tables = %w[holdings_cluster holdings_cluster_oclc holdings_cluster_htitem_jn]
+  tables.each do | t |
     count_q = "SELECT COUNT(*) AS c FROM #{t}"
     count_r = run_single_query(count_q)
     puts "#{count_q} -- gave #{count_r} rows"
-    trunc_q = "TRUNCATE #{t}" 
+    trunc_q = "TRUNCATE #{t}"
     puts trunc_q
     @conn.execute(trunc_q)
   end
@@ -88,74 +126,78 @@ end
 
 def cluster_main
   puts "Grabbing volume_ids..."
-  query1 = "SELECT DISTINCT(volume_id) FROM holdings_htitem LIMIT 0,5000"
+  query1 = "SELECT DISTINCT(volume_id) FROM holdings_htitem"
   all_vids = run_list_query(query1)
   puts "#{all_vids.size} ids received..."
-  
+
   viter = 0
   all_vids.each do |vid|
     viter += 1
     next if vid.length < 3
 
-    ## get the OCNs for each volume_id ##
+    # get the OCNs for each volume_id
     query3 = "SELECT oclc FROM holdings_htitem_oclc WHERE volume_id = '#{vid}'"
     ocns = run_list_query(query3)
     # skip htitems with no oclc number
     next unless ocns.any?
-
-    # are any OCNs already participating in other clusters? #
-    pclusters = []
-    ocns.each do |ocn|
-      if @oclc_cluster_d.key?(ocn)
-        cid = @oclc_cluster_d[ocn]
-        pclusters << cid
-      end
-    end
-    pclusters.uniq!
-
+    pclusters = clusters_with_ocns(ocns)
     # if yes, aggregate
-    if pclusters.size > 0
+
+    # If the ocn(s) is/aren't in a cluster, make a new cluster.
+    if pclusters.size == 0 then
+      create_cluster(ocns, vid)
+    else
+      # If any of the ocns already are in a cluster, merge the clusters.
       cids = pclusters
       cids.sort!
       # todo: test this
       lowest_cid = cids.pop
-      query4 =  "INSERT INTO holdings_cluster_htitem_jn (cluster_id, volume_id) \ 
-                  VALUES (#{lowest_cid}, '#{vid}')" 
-      puts query4
+      query4 =  "INSERT INTO holdings_cluster_htitem_jn (cluster_id, volume_id) \
+                  VALUES (#{lowest_cid}, '#{vid}')"
       @conn.execute(query4)
 
       # add all OCNs to lowest matching cluster number
       ocns.each do |ocn|
         @oclc_cluster_d[ocn] = lowest_cid
         @cluster_oclc_d[lowest_cid] << ocn
-      end  
+      end
       @cluster_oclc_d[lowest_cid].uniq!
       # merge remaining clusters into root cluster
       cids.each do |cid|
         merge_clusters(lowest_cid, cid.to_i)
+        # do not leave in
+        dump_data_structure()
+        exit
       end
-    else
-      create_cluster(ocns, vid)
     end
 
     if viter % 1000 == 0
       @log.i viter
-    end      
- 
-  end 
+    end
+  end
 
-  @log.i "viter: #{viter}" 
+  @log.i "viter: #{viter}"
   puts "dumping final data structure"
   dump_data_structure()
- 
+
+end
+
+# are any OCNs already participating in other clusters?
+def clusters_with_ocns (ocns)
+  pclusters = []
+  ocns.each do |ocn|
+    if @oclc_cluster_d.key?(ocn)
+      cid = @oclc_cluster_d[ocn]
+      pclusters << cid
+    end
+  end
+  return pclusters.uniq  
 end
 
 def load_table
-
-end
-
-def get_loadfile_path
-  
+  q = "LOAD DATA LOCAL INFILE '#{@outfn.path}' INTO TABLE holdings_cluster_oclc"
+  @log.i(q)
+  @conn.execute(q)
 end
 
 # Exports one of the table data structures to a flatfile.  Structs are
@@ -165,16 +207,16 @@ def dump_data_structure
     oclcs.each do |o|
       @outfn.file.puts "#{cid}\t#{o}"
     end
-  end   
+  end
 end
 
 if $0 == __FILE__ then
-  setup();
+  setup()
   # Start with blank slate.
   truncate_tables()
   # Calculate clusters and write to file.
   cluster_main()
   # Load file into holdings_cluster_oclc.
-  load_table()                             
-  teardown();
+  load_table()
+  teardown()
 end
--- lib/hathienv.rb
+++ lib/hathienv.rb
@@ -1,3 +1,6 @@
+require 'java'
+java_import 'java.lang.Runtime'
+
 module Hathienv
   class Env
 
@@ -9,6 +12,17 @@ module Hathienv
       return %x(hostname).include?('grog');
     end
 
+    def Env.require_minimum_ram (needed_mb)    
+      mxm = Runtime.getRuntime.maxMemory.to_i;
+      # byte to megabyte conversion
+      mxm_mb = mxm / 1048576;
+      $stderr.puts "#{$0}: Required: #{needed_mb} mb, available #{mxm_mb} mb";
+      if needed_mb > mxm_mb then
+        return false;
+      end
+      return true;
+    end
+    
   end
 end
 
@@ -16,4 +30,6 @@ if $0 == __FILE__ then
   puts "Testing Hathienv:";
   puts "Dev? "  + Hathienv::Env.is_dev?().to_s;
   puts "Prod? " + Hathienv::Env.is_prod?().to_s;
+  puts Hathienv::Env.require_minimum_ram(100);
+  puts Hathienv::Env.require_minimum_ram(1000);
 end
