e2b13b39a95697b1e74579ed97d0acbea378b4cb
Martin Warin <mwarin@umich.edu>
2018-03-28 12:25:02 -0400

    using prepared queries

--- experimental/step_03.rb
+++ experimental/step_03.rb
@@ -1,14 +1,16 @@
 require 'hathidata'
 require 'hathidb'
 require 'hathilog'
-
-# todos:
-# rename queries
-# prepare queries
+require 'hathienv'
 
 # first merge_clusters happens at viter=1780134
 
 def setup
+  if !Hathienv::Env.require_minimum_ram(6000) then
+    # I know, 6000 != 7000, but the JVM and/or JRuby itself eats a bunch.
+    # Or the math in Hathienv is wrong.
+    raise "Not enough memory to do this. Use -J-Xmx7000m or higher."
+  end
   @log = Hathilog::Log.new()
   @log.i("Started")
 
@@ -16,6 +18,17 @@ def setup
   @conn  = db.get_conn()
   @outfn = Hathidata::Data.new('builds/current/cluster_oclc.data_rb').open('w')
 
+  # These get called all the time, so worth preparing.
+  @queries = {
+    :create_cluster_q       => prep_q('INSERT INTO holdings_cluster (cost_rights_id, osici, last_mod) VALUES (0, ?, SYSDATE())'),
+    :insert_hchj            => prep_q('INSERT INTO holdings_cluster_htitem_jn (cluster_id, volume_id) VALUES (?, ?)'),
+    :get_volid_from_cluster => prep_q('SELECT volume_id FROM holdings_cluster_htitem_jn WHERE cluster_id = ?'),
+    :del_hchj               => prep_q('DELETE FROM holdings_cluster_htitem_jn WHERE cluster_id = ?'),
+    :del_hc                 => prep_q('DELETE FROM holdings_cluster WHERE cluster_id = ?'),
+    :get_oclc_from_volid    => prep_q('SELECT oclc FROM holdings_htitem_oclc WHERE volume_id = ?'),
+    :last_id                => prep_q('SELECT LAST_INSERT_ID() AS id')
+  }
+  
   @cluster_oclc_d = {}
   @oclc_cluster_d = {}
 end
@@ -26,30 +39,37 @@ def teardown
   @log.i("Finished")
 end
 
-# runs query, returns array of items
-def run_list_query(query)
-  items = []
-  @conn.enumerate(query) do |row|
-    items << row[0]
-  end
-  return items
+def prep_q(sql)
+  {:sql => sql, :prep => @conn.prepare(sql)}
 end
 
-# Run a generic query, return first row
-def run_single_query(query)
-  @conn.enumerate(query) do |row|
-    return row[0]
+# runs query, returns array of items
+# pass it a query string or a resultset
+def run_list_query(query)
+  items = []
+  if query.class == String then
+    @conn.enumerate(query) do |row|
+      items << row[0]
+    end
+  elsif query.class == JDBCHelper::Connection::ResultSet then
+    query.each do |row|
+      items << row[0]
+    end
   end
+
+  return items
 end
 
 # creates a new cluster and populates tables appropriately
 def create_cluster(ocns, vol_id)
   # insert into cluster, get id
   ocn0 = ocns[0]
-  query2 = "INSERT INTO holdings_cluster (cost_rights_id, osici, last_mod) \
-            VALUES (0, '#{ocn0}', SYSDATE())"
-  @conn.execute(query2)
-  pkid = run_single_query("SELECT LAST_INSERT_ID()")
+  @queries[:create_cluster_q][:prep].execute(ocn0)
+  # get last inserted id
+  pkid = nil
+  @queries[:last_id][:prep].enumerate() do |row|
+    pkid = row[:id]
+  end
 
   # insert OCNs into cluster_oclc tables
   ocns.each do |nocn|
@@ -66,10 +86,7 @@ def create_cluster(ocns, vol_id)
     end
   end
 
-  # insert volume_id into cluster_htitem_jn
-  query4 = "INSERT INTO holdings_cluster_htitem_jn (cluster_id, volume_id) \
-            VALUES ('#{pkid}', '#{vol_id}')"
-  @conn.execute(query4)
+  @queries[:insert_hchj][:prep].execute(pkid, vol_id)
   return pkid
 end
 
@@ -79,19 +96,13 @@ end
 def merge_clusters (cid1, cid2)
   @log.d("Merging cluster id #{cid1}  (ocns #{@cluster_oclc_d[cid1].join(',')})")
   @log.d("... with cluster id #{cid2} (ocns #{@cluster_oclc_d[cid2].join(',')})")
-
-  queryc1a = "SELECT volume_id FROM holdings_cluster_htitem_jn WHERE cluster_id = '#{cid1}'"
-  queryc2a = "SELECT volume_id FROM holdings_cluster_htitem_jn WHERE cluster_id = '#{cid2}'"
-  c1vids = run_list_query(queryc1a)
-  c2vids = run_list_query(queryc2a)
+  c1vids = run_list_query(@queries[:get_volid_from_cluster][:prep].enumerate(cid1))
+  c2vids = run_list_query(@queries[:get_volid_from_cluster][:prep].enumerate(cid2))
 
   c2vids.each do |vid|
     unless c1vids.include?(vid) then
-      mcquery2 = "INSERT INTO holdings_cluster_htitem_jn (cluster_id, volume_id) \
-                           VALUES ('#{cid1}', '#{vid}')"
-
-      @log.i(mcquery2)
-      @conn.execute(mcquery2)
+      @queries[:insert_hchj][:prep].execute(cid1, vid)
+      @log.i(@queries[:insert_hchj][:sql] + " -- #{cid1}, #{vid}")
     end
   end
 
@@ -104,11 +115,8 @@ def merge_clusters (cid1, cid2)
   @cluster_oclc_d[cid1].uniq!
   @cluster_oclc_d.delete(cid2)
 
-  mcquery5a = "DELETE FROM holdings_cluster_htitem_jn WHERE cluster_id = '#{cid2}'"
-  mcquery5c = "DELETE FROM holdings_cluster WHERE cluster_id = '#{cid2}'"
-
-  @conn.execute(mcquery5a)
-  @conn.execute(mcquery5c)  
+  @queries[:del_hchj][:prep].execute(cid2)
+  @queries[:del_hc][:prep].execute(cid2)
 end
 
 # Get number of records in each table, then truncate
@@ -116,7 +124,10 @@ def truncate_tables
   tables = %w[holdings_cluster holdings_cluster_oclc holdings_cluster_htitem_jn]
   tables.each do | t |
     count_q = "SELECT COUNT(*) AS c FROM #{t}"
-    count_r = run_single_query(count_q)
+    count_r = nil
+    @conn.query("SELECT COUNT(*) AS c FROM #{t}") do |row|
+      count_r = row[:c]
+    end
     puts "#{count_q} -- gave #{count_r} rows"
     trunc_q = "TRUNCATE #{t}"
     puts trunc_q
@@ -135,9 +146,8 @@ def cluster_main
     viter += 1
     next if vid.length < 3
 
-    # get the OCNs for each volume_id
-    query3 = "SELECT oclc FROM holdings_htitem_oclc WHERE volume_id = '#{vid}'"
-    ocns = run_list_query(query3)
+    # get the OCNs for each volume_id  
+    ocns = run_list_query(@queries[:get_oclc_from_volid][:prep].enumerate(vid))
     # skip htitems with no oclc number
     next unless ocns.any?
     pclusters = clusters_with_ocns(ocns)
@@ -150,11 +160,8 @@ def cluster_main
       # If any of the ocns already are in a cluster, merge the clusters.
       cids = pclusters
       cids.sort!
-      # todo: test this
       lowest_cid = cids.pop
-      query4 =  "INSERT INTO holdings_cluster_htitem_jn (cluster_id, volume_id) \
-                  VALUES (#{lowest_cid}, '#{vid}')"
-      @conn.execute(query4)
+      @queries[:insert_hchj][:prep].execute(lowest_cid, vid)
 
       # add all OCNs to lowest matching cluster number
       ocns.each do |ocn|
@@ -179,7 +186,6 @@ def cluster_main
   @log.i "viter: #{viter}"
   puts "dumping final data structure"
   dump_data_structure()
-
 end
 
 # are any OCNs already participating in other clusters?
