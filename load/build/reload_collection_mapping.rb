960710df0de63915ba6b6c9483c05b6bdc20c918
Martin Warin <mwarin@umich.edu>
2018-08-17 11:00:45 -0400

    did some changes, forgot why, committed them

--- load/build/reload_collection_mapping.rb
+++ load/build/reload_collection_mapping.rb
@@ -10,13 +10,26 @@ db = Hathidb::Db.new();
 dc = db.get_conn();
 pc = db.get_prod_conn();
 
+# Got to make sure we don't get mappings for members that don't exist in dev.
+get_target_member_ids = "SELECT member_id FROM holdings_htmember";
+member_ids = [];
+dc.query(get_target_member_ids) do |row|
+  member_ids << row[:member_id];
+end
+
 del_sql = "TRUNCATE TABLE hathi_collection_map";
-get_source_sql = "SELECT collection, billing_entity FROM ht_collections ORDER BY collection";
+
+get_source_sql = %W<
+  SELECT collection, billing_entity
+  FROM ht_collections
+  WHERE billing_entity IN (#{member_ids.map{|x| '?'}.join(',')})
+  ORDER BY collection
+>.join(' ');
 get_target_sql = "SELECT collection, member_id FROM hathi_collection_map ORDER BY collection";
 ins_sql = "INSERT INTO hathi_collection_map (collection, member_id) VALUES (?,?)";
 
 get_target_q = dc.prepare(get_target_sql)
-del_q = dc.prepare(del_sql); 
+del_q = dc.prepare(del_sql);
 get_source_q = pc.prepare(get_source_sql);
 ins_q = dc.prepare(ins_sql);
 
@@ -31,7 +44,7 @@ end
 del_q.execute();
 
 # Get each record from prod and insert in dev.
-get_source_q.enumerate() do |row|
+get_source_q.enumerate(*member_ids) do |row|
   collection = row[:collection];
   member_id  = row[:billing_entity];
   log.d("#{collection} => #{member_id}");
