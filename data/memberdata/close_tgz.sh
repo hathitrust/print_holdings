78002d92f68032949e20f91a5d6c5fc6c86d659d
Martin Warin <mwarin@umich.edu>
2018-10-24 13:25:11 -0400

    now takes a list of members, instead of just a single member

--- data/memberdata/close_tgz.sh
+++ data/memberdata/close_tgz.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-member=`ls | grep -Po "^$1(\.estimate)?$"`;
-
-if [ -z "$member" ]; then
-    echo "Could not find member dir for $1";
-else
-    echo "Closing $member";
-    tar --remove-files -cvzf $member.tar.gz $member/* && rmdir $member;
-    echo "Done.";
-fi
-
+for var in "$@";
+do :
+   member=`ls | grep -Po "^$var(\.estimate)?$"`;
+   if [ -z "$member" ]; then
+       echo "Could not find member dir for $var";
+   else
+       echo "Closing $member";
+       tar --remove-files -cvzf $member.tar.gz $member/* && rmdir $member;
+       echo "Done.";
+   fi
+done
+	   
