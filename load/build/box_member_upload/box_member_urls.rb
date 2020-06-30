a14543124c5df6082463e0e7f1b6ccb90de8cab6
Martin Warin <mwarin@umich.edu>
2020-06-30 12:50:13 -0400

    changed for new box folder structure

--- load/build/box_member_upload/box_member_urls.rb
+++ load/build/box_member_upload/box_member_urls.rb
@@ -5,28 +5,61 @@ require 'hathiquery'
 db   = Hathidb::Db.new()
 conn = db.get_conn()
 
+# Get members from db so we can see if anyone is missing
 all_members = {}
 conn.query(Hathiquery.get_active_members) do |row|
   all_members[row[:member_id]] = true
 end
+all_members.delete("hathitrust")
 
-result = `curl -s -n --list-only "ftps://ftp.box.com/Member Data/"`
+# Get shared print members so we can see that they have shared print subfolders
+shared_print_members = {}
+conn.query(Hathiquery.get_shared_print_members) do |row|
+  shared_print_members[row[:member_id]] = true
+end
+
+TOP_FOLDER  = "ftps://ftp.box.com/Member Data"
+SUB_FOLDERS = ["analysis", "print holdings", "shared print"]
+
+# Get all folders under top folder
+result = `curl -s -n --list-only "#{TOP_FOLDER}/"`
 result.each_line do |line|
   line.strip!
   if line.match(/^[a-z\-]+$/)
-
-    if all_members.key?(line) then
+    member_id = line
+    if all_members.key?(member_id) then
       # Deleting seen members from the hash so we can see if there are any left
       # i.e. missing a box folder.
-      all_members.delete(line)
+      all_members.delete(member_id)
     else
-      warn "#{line} is not a member"
-    end    
-    puts "#{line}\tftps://ftp.box.com/Member Data/#{line}/"
+      # These should be moved out manually.
+      warn "#{member_id} is not an active member, please move out manually"
+      next
+    end
+
+    # Check make sure each folder contains the subfolders
+    # Create missing subfolders
+    # (only create shared print if they are a shared print member)
+    sub_result = `curl -s -n --list-only "#{TOP_FOLDER}/#{member_id}/"`.split("\n")
+
+    SUB_FOLDERS.each do |subf|
+      if !sub_result.include?(subf) then
+        if subf == "shared print" && !shared_print_members.key?(member_id) then
+          # Member is missing a "shared print" subfolder,
+          # but is not a shared print member, so let it slide.
+          next
+        end
+        warn "Member #{member_id} is missing subfolder #{subf}. Creating it now..."
+        # Create sub-folder
+        res = `curl -n --ftp-create-dirs "#{TOP_FOLDER}/#{member_id}/#{subf}/"`
+        warn res
+      end
+    end
+    # Output to file:
+    puts "#{member_id}\t#{TOP_FOLDER}/#{member_id}/"
   end
 end
 
 all_members.keys.each do |member|
   warn "did not see a box for member #{member}"
 end
-
--- load/build/box_member_upload/upload_to_member_folder.sh
+++ load/build/box_member_upload/upload_to_member_folder.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# Usage:
+# bash upload_to_member_folder.sh <member_id> <file> <remote_subfolder>
+
+# Example:
+# bash upload_to_member_folder.sh umich monos.tsv "print holdings"
+# ... will upload the file monos.tsv to the subfolder "print holdings" in the umich box.
+
 member_id=$1
 upload_file=$2
+subfolder=$3
 
-upload_path=$(egrep "^$member_id	" member_urls.txt | cut -f 2)
+if [[ ! $subfolder =~ ^(print holdings|shared print|analysis|test)$ ]]
+then
+    echo "param 3 must match ^(print holdings|shared print|analysis)$"
+    echo "\"$subfolder\" does not"
+fi
 
-curl -v -n -T $upload_file "$upload_path"
+upload_path=$(grep -P "^$member_id\t" member_urls.txt | cut -f 2)
+echo "Uploading $upload_file to $upload_path$subfolder/"
+curl -n -T $upload_file "$upload_path$subfolder/"
