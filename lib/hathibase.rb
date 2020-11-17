79122c49c26e5157469b53c2bc4974ebf872ceae
Martin Warin <mwarin@umich.edu>
2020-11-17 11:31:09 -0500

    added rows()

--- lib/hathibase.rb
+++ lib/hathibase.rb
@@ -5,26 +5,33 @@ require 'hathilog';
 module Hathibase
   class BaseReport
     def initialize
-      puts "base class being initialized"
       @log  = Hathilog::Log.new();
       @db   = Hathidb::Db.new();
       @conn = @db.get_conn();
     end
 
+    # Yields 1 row at a time from an executed sql.
+    def rows(sql)
+      q = @conn.prepare(sql);
+      q.enumerate do |row|
+        yield row;
+      end
+    end
+    
     # Takes a sql string and a filename string.
     # Runs query and puts results in file.
-    def output_query_to_file(sql, fn)
+    # Returns the HathiData object it wrote to.
+    def output_query_to_file(sql, fn, header = true)
       hdout  = Hathidata::Data.new(fn).open('w');
-      header = true;
-      q = @conn.prepare(sql)
-      q.enumerate do |row|
+      rows(sql) do |row|
         if header then
           hdout.file.puts(row.to_h.keys.join("\t"));
           header = false;
         end
-        hdout.file.puts(row.to_a.join("\t"))
+        hdout.file.puts(row.to_a.join("\t"));
       end
       hdout.close();
+      return hdout
     end    
   end
 end
