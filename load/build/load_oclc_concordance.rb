9a76bd99cf85b6082395362a1a587eda8d48db50
Martin Warin <mwarin@umich.edu>
2020-11-17 11:33:27 -0500

    print counts before and after

--- load/build/load_oclc_concordance.rb
+++ load/build/load_oclc_concordance.rb
@@ -2,13 +2,30 @@ require 'hathidata';
 require 'hathidb';
 require 'hathilog';
 
+# set up
 log  = Hathilog::Log.new();
 db   = Hathidb::Db.new();
 conn = db.get_conn();
+
+# count
+count_sql = "SELECT COUNT(*) FROM oclc_concordance";
+puts "count before:";
+conn.query(count_sql) do |row|
+  puts row.to_a.join("\t");
+end
+
+# update
 hd   = Hathidata::Data.new("oclc_concordance/concordance_1_ne_2.txt");
 q    = "LOAD DATA LOCAL INFILE '#{hd.path.to_s}' INTO TABLE oclc_concordance";
-
 log.d(q);
 conn.execute(q);
+
+# count
+puts "after:";
+conn.query(count_sql) do |row|
+  puts row.to_a.join("\t");
+end
+
+# shut down
 conn.close();
 log.d("Finished");
