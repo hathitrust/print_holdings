a02745a0cc00fb60a203e2ed345cfda22c02be0e
Martin Warin <mwarin@umich.edu>
2020-11-17 11:44:50 -0500

    script to mass-update resolved_oclc for holdings/commitments/both

---
+++ load/build/reload_resolved_oclc.rb
@@ -0,0 +1,68 @@
+require 'hathidb';
+require 'hathilog';
+
+=begin
+
+Reloads the resolved_oclc field for holdings, commitments, or both.
+Run under db_auth_nohup.sh, because it's going to take a while.
+Pass "holdings" and/or "commitments" as args to reload their ocns.
+E.g.
+  $ bash under db_auth_nohup.sh ruby reload_resolved_oclc.rb holdings
+  $ bash under db_auth_nohup.sh ruby reload_resolved_oclc.rb commitments
+  $ bash under db_auth_nohup.sh ruby reload_resolved_oclc.rb holdings commitments
+
+=end
+
+class Reloader
+  def initialize(args)
+    @args = args;
+    @log  = Hathilog::Log.new();    
+    db    = Hathidb::Db.new();
+    @conn = db.get_interactive();
+
+    @update_holdings_sql = %W<
+      UPDATE holdings_memberitem 
+      SET resolved_oclc = COALESCE(
+        (SELECT MAX(o.resolved) FROM oclc_concordance AS o WHERE o.variant = oclc),
+        oclc
+      )
+    >.join(' ');
+
+    @update_commitments_sql = %W<
+      UPDATE shared_print_commitments 
+      SET resolved_oclc = COALESCE(
+        (SELECT MAX(o.resolved) FROM oclc_concordance AS o WHERE o.variant = local_oclc),
+        local_oclc
+      )
+    >.join(' ');
+  end
+  
+  def run  
+    @log.i("Started");
+    if @args.include?("holdings") then    
+      @log.i("updating holdings...");
+      do_update(@update_holdings_sql)
+      @log.i("done updating holdings");
+    end  
+
+    if @args.include?("commitments") then
+      @log.i("updating commitments...");
+      do_update(@update_commitments_sql);
+      @log.i("done updating commitments");
+    end
+
+    @log.i("Finished");
+  end
+
+  def do_update(sql)
+    @log.i(sql);
+    update_q = @conn.prepare(sql);
+    # update_q.execute();
+  end
+  
+end
+
+if $0 == __FILE__ then
+  r = Reloader.new(ARGV);
+  r.run();
+end
